e0b88aaf8f579166fafc2d0ca112eb91d1f2b2de
Siyamak_Plaza <habibipour@gilertebat.com>
2020-09-28 16:27:19 +0330

    add new parameter

--- src/providers/theme/theme.light.js
+++ src/providers/theme/theme.light.js
@@ -6,12 +6,69 @@ const palette = {
     error: '#FF4A6D',
     success: '#42D9BB',
     stroke: '#D8D8D8',
-    text: '#2B273C'
+    onSurface: '#2B273C',
+    subtitle: '#CCCCCC',
+    unselected: '#BBBAC0',
+    strokeVariant: '#F3F3F3',
+    surface: '#F7F7F7',
+
+    greenUser: '#42D9BB',
+    silverUser: '#BBBAC0',
+    goldenUser: '#FBC635',
+    diamondUser: '#896FFA'
 };
 
+const dim = {
+    1: "8px",
+    2: "16px",
+    3: "24px",
+    4: "32px",
+    5: "40px"
+}
+
+const typography = {
+
+    headline1:`font-size: 24px;
+                font-weight: bold;
+                line-height: 1.5;`,
+
+    headline2:`font-size: 18px;
+                font-weight: bold;
+                line-height: 1.5;`,
+
+    headline3:`font-size: 16px;
+                font-weight: bold;
+                line-height: 1.5;`,
+
+    bodyMdNormal:`font-size: 16px;
+                font-weight: normal;
+                line-height: 1.5;`,
+
+    bodySmBold:`font-size: 14px;
+                font-weight: bold;
+                line-height: 1.5;`,
+
+    bodySmNormal:`font-size: 14px;
+                font-weight: normal;
+                line-height: 1.5;`
+
+}
+
+const defaults = {
+    screenWideSize: '1600px',
+    screenSideDim: dim[1],
+    borderRadius: '3px',
+    direction: 'rtl'
+}
+
 const theme = {
-    palette
+    palette,
+    dim,
+    typography,
+    defaults
 };
+
+
 
 
 export { theme };
