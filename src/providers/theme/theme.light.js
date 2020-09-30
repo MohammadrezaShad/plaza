96095ab38ccb93b940bf369aed4c63ec5be599d4
Siyamak_Plaza <habibipour@gilertebat.com>
2020-09-30 16:18:06 +0330

    add new fields

--- src/providers/theme/theme.light.js
+++ src/providers/theme/theme.light.js
@@ -1,22 +1,42 @@
 const palette = {
     back: '#FFFFFF',
+    onBack: '#2B273C',
     primary: '#F89A01',
+    onPrimary: '#FFFFFF',
     secondary: '#03A9F4',
-    surface: '#03A9F4',
-    error: '#FF4A6D',
-    success: '#42D9BB',
-    stroke: '#D8D8D8',
-    onSurface: '#2B273C',
-    subtitle: '#CCCCCC',
-    unselected: '#BBBAC0',
-    strokeVariant: '#F3F3F3',
+    onSecondary: '#FFFFFF',
     surface: '#F7F7F7',
+    onSurface: '#2B273C',
+    error: '#FF4A6D',
+    onError: '#FFFFFF',
+    success: '#42D9BB',
+    onSuccess: '#FFFFFF',
+    disabled: '#BBBAC0',
+    onDisabled: 'FFFFFF',
+    stroke: '#D8D8D8',
+    subtitle: '#757280',
+    strokeVariant: '#F3F3F3',
 
     greenUser: '#42D9BB',
     silverUser: '#BBBAC0',
     goldenUser: '#FBC635',
     diamondUser: '#896FFA'
 };
+
+const getUserLevelColor = (level) => {
+    switch (level){
+        case 1:
+            return palette.greenUser;
+        case 2:
+            return palette.silverUser;
+        case 3:
+            return palette.goldenUser;
+        case 4:
+            return palette.diamondUser;
+        default:
+            return palette.greenUser;
+    }
+}
 
 const dim = {
     1: "8px",
@@ -50,22 +70,28 @@ const typography = {
 
     bodySmNormal:`font-size: 14px;
                 font-weight: normal;
-                line-height: 1.5;`
+                line-height: 1.5;`,
 
+    subtitleXsNormal:`font-size: 12px;
+                font-weight: normal;
+                line-height: 1.5;`
 }
 
 const defaults = {
     screenWideSize: '1600px',
-    screenSideDim: dim[1],
-    borderRadius: '3px',
-    direction: 'rtl'
+    screenSideDim: dim[2],
+    borderRadius: '4px',
+    direction: 'rtl',
+    boxShadow:'0 6px 12px rgba(0,0,0,.05)',
+    shortBoxShadow:'0 3px 6px rgba(0,0,0,.05)'
 }
 
 const theme = {
     palette,
     dim,
     typography,
-    defaults
+    defaults,
+    getUserLevelColor
 };
 
 
