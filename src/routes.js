01a47b0087168beb5011a422cad3e3b541b3270b
Siyamak_Plaza <habibipour@gilertebat.com>
2020-09-30 16:14:20 +0330

    fix old routes - add product route

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,27 @@
 import MainLayout from './components/layouts/main-layout';
 import Home from './pages';
 import Signin from './pages/signin';
+import ProductDetail from './pages/product';
 import Paths from './utils/paths';
 
 
 
 const routes = [
     {
-        path: Paths.signin(),
+        path: Paths.signin.getRoute(),
         component: Signin
     },
     {
         component: MainLayout,
         routes: [
             {
-                path: "/",
+                path: Paths.home.getRoute(),
                 exact: true,
                 component: Home
+            },
+            {
+                path: Paths.product.detail().getRoute(),
+                component: ProductDetail
             }
         ]
     }
