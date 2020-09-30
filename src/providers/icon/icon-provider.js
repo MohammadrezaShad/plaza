25db14ce25d4e0dd234267aa3f65f1394b94c20b
Siyamak_Plaza <habibipour@gilertebat.com>
2020-09-30 16:17:33 +0330

    add propTypes

--- src/providers/icon/icon-provider.js
+++ src/providers/icon/icon-provider.js
@@ -2,9 +2,18 @@ import React from 'react'
 import IcoMoon from 'react-icomoon'
 import iconSet from '../../assets/fonts/icon/iconmoon.json';
 
+import PropTypes from 'prop-types'
+
 const IconProvider = ({ ...props }) => {
     return <IcoMoon iconSet={iconSet} {...props} />;
 }
 
+IconProvider.propTypes = {
+    icon: PropTypes.string.isRequired,
+    size: PropTypes.string
+}
+
 export default IconProvider
+
+
 
