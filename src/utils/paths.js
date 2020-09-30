3d1a5c3241e273c2dc03bbe53c2dc199065fb658
Siyamak_Plaza <habibipour@gilertebat.com>
2020-09-30 16:17:46 +0330

    refactor

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -1,6 +1,59 @@
-const Paths = {
-    home: () => '/',
-    signin: () => '/signin'
+class Uri {
+    constructor(url, route) {
+        this.url = url;
+        this.route = route || url;
+    }
+
+    getRoute() {
+        return this.route
+    }
+
+    getPathAndQuery() {
+        return this.url
+    }
+
+    getPath() {
+        // TODO : remove querystring and return path
+        return this.url
+    }
+
+    getAbsoulteUri() {
+        return process.env.DOMAIN + this.url;
+    }
 }
+
+class Paths {
+
+    static home = new Uri('/')
+    
+    static signin = new Uri('/login')
+    
+
+    static club = class {
+        static base = "/club"
+
+        static home = new Uri(this.base)
+    }
+
+    static profile = class {
+        static base = "/profile"
+
+        static home = new Uri(this.base)
+        static orders = new Uri(this.base + '/orders')
+    }
+
+    static product = class {
+        static detail (id) { 
+            return new Uri(`/product/${id}`,'/product/:id')
+        };
+
+        static search () {
+            return new Uri('/search');
+        }
+    }
+}
+
+
+
 
 export default Paths
