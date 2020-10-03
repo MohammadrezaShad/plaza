2474aa08d002c22dd539d4970066c24cf692a816
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-03 08:21:01 +0330

    add show modal

--- src/components/views/home/client/home.page.jsx
+++ src/components/views/home/client/home.page.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuickView from '../../../dialogs/product';
+import Dialog from '../../../shared/dialog';
 
 import {
     Title
 } from './home.styled';
 
 
+
+
 const HomePage = () => {
+    const [modalIsShow, setModalIsShow] = useState(false);
+
+    const modalCloseHandler = () => {
+        setModalIsShow(false);
+    }
+
     return (
         <div>
             <Title>home client</Title>
+            
+            <button onClick={() => {setModalIsShow(true)}}>show modal</button>
             <div style={{height:'1000px'}}></div>
+            <QuickView open={modalIsShow} onClose={modalCloseHandler} />
         </div>
     )
 }
