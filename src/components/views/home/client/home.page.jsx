b61d48300baf4719f4a90677df8d281a88ee2353
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-05 11:15:30 +0330

    add testing content

--- src/components/views/home/client/home.page.jsx
+++ src/components/views/home/client/home.page.jsx
@@ -21,8 +21,59 @@ const HomePage = () => {
             <Title>home client</Title>
             
             <button onClick={() => {setModalIsShow(true)}}>show modal</button>
-            <div style={{height:'1000px'}}></div>
             <QuickView open={modalIsShow} onClose={modalCloseHandler} />
+            <h2>test1</h2>
+            <h2>test2</h2>
+            <h2>test3</h2>
+            <h2>test4</h2>
+            <h2>test5</h2>
+            <h2>test6</h2>
+            <h2>test7</h2>
+            <h2>test8</h2>
+            <h2>test9</h2>
+            <h2>test10</h2>
+            <h2>test1</h2>
+            <h2>test2</h2>
+            <h2>test3</h2>
+            <h2>test4</h2>
+            <h2>test5</h2>
+            <h2>test6</h2>
+            <h2>test7</h2>
+            <h2>test8</h2>
+            <h2>test9</h2>
+            <h2>test10</h2>
+            <h2>test1</h2>
+            <h2>test2</h2>
+            <h2>test3</h2>
+            <h2>test4</h2>
+            <h2>test5</h2>
+            <h2>test6</h2>
+            <h2>test7</h2>
+            <h2>test8</h2>
+            <h2>test9</h2>
+            <h2>test10</h2>
+            <h2>test1</h2>
+            <h2>test2</h2>
+            <h2>test3</h2>
+            <h2>test4</h2>
+            <h2>test5</h2>
+            <h2>test6</h2>
+            <h2>test7</h2>
+            <h2>test8</h2>
+            <h2>test9</h2>
+            <h2>test10</h2>
+            <h2>test1</h2>
+            <h2>test2</h2>
+            <h2>test3</h2>
+            <h2>test4</h2>
+            <h2>test5</h2>
+            <h2>test6</h2>
+            <h2>test7</h2>
+            <h2>test8</h2>
+            <h2>test9</h2>
+            <h2>test10</h2>
+           
+            <div style={{height:'1000px'}}></div>
         </div>
     )
 }
