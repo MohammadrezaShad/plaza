46cf5729e9d810425e47ec15f001830b338c98e7
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-10 15:25:47 +0330

    add new features

--- src/components/layouts/main-layout/client/header/search-box/search-results/last-searches/last-searches.jsx
+++ src/components/layouts/main-layout/client/header/search-box/search-results/last-searches/last-searches.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useTranslation } from "react-i18next";
 
 import {
-    Wrapper
+    Wrapper,
+    Head,
+    List,
+    Item,
+    ItemLink,
+    Icon,
+    Text
 } from './last-searches.styled'
+import IconProvider from '../../../../../../../../providers/icon/icon-provider';
+import Paths from '../../../../../../../../utils/paths';
 
 const LastSearches = props => {
+    const { t } = useTranslation();
+
     return (
         <Wrapper>
-            
+            <Head>{t("lastSearches")}</Head>
+            <List>
+                <Item>
+                    <ItemLink to={Paths.home.getPath()}>
+                    <Icon as={IconProvider} icon="recently" size="20px" />
+                    <Text>hdmi</Text>
+                    </ItemLink>
+                </Item>
+                <Item>
+                    <ItemLink to={Paths.home.getPath()}>
+                    <Icon as={IconProvider} icon="recently" size="20px" />
+                    <Text>گلس سامسونگ A50</Text>
+                    </ItemLink>
+                </Item>
+                <Item>
+                    <ItemLink to={Paths.home.getPath()}>
+                    <Icon as={IconProvider} icon="recently" size="20px" />
+                    <Text>موس گیمینگ</Text>
+                    </ItemLink>
+                </Item>
+            </List>
         </Wrapper>
     )
 }
