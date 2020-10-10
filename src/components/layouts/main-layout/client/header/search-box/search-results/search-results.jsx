8d9afff28c5dd4256eafdce35d983600dc941c39
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-10 15:25:16 +0330

    add ResultsList LastSearches MostSearches

--- src/components/layouts/main-layout/client/header/search-box/search-results/search-results.jsx
+++ src/components/layouts/main-layout/client/header/search-box/search-results/search-results.jsx
@@ -6,11 +6,17 @@ import {
     Container
 } from './search-results.styled'
 import EmptyResult from './empty-result'
+import LastSearches from './last-searches'
+import MostSearches from './most-searches'
+import ResultsList from './results-list'
 
 const SearchResults = ({results,term}) => {
     return (
         <Wrapper>
             <Container>
+                <ResultsList />
+                <LastSearches />
+                <MostSearches />
                 <EmptyResult />
             </Container>
         </Wrapper>
