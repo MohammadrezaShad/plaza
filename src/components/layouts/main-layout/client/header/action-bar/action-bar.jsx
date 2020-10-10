285aff241afdc7d277c0ea31594957aa10aef177
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-10 13:31:41 +0330

    add memo

--- src/components/layouts/main-layout/client/header/action-bar/action-bar.jsx
+++ src/components/layouts/main-layout/client/header/action-bar/action-bar.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import {
     Wrapper
 } from './action-bar.styled'
 import CartMenu from './cart-menu'
-import ProfileMenu from './profile-menu'
+import ProfileMenu from './profile-menu/profile-menu-container'
 import WholesalingMenu from './wholesaling-menu'
 
-const ActionBar = () => {
+const ActionBar = React.c(() => {
     return (
         <Wrapper>
             <WholesalingMenu />
             <CartMenu/>
-            <ProfileMenu/>
+            <ProfileMenu isSignedIn={true}/>
         </Wrapper>
     )
-}
+})
 
 export default ActionBar
