130105a70a895f6a183b9624d15de79612e9885c
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-10 13:35:15 +0330

    add first content

--- src/components/layouts/main-layout/client/header/nav-bar/nav-item/nav-submenu/nav-submenu-item/nav-submenu-item.jsx
+++ src/components/layouts/main-layout/client/header/nav-bar/nav-item/nav-submenu/nav-submenu-item/nav-submenu-item.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import {
+    StyledWrapper,
+    StyledCol
+} from './nav-submenu-item.styled'
+import NavSubmenuItems from './nav-submenu-items'
+
 function NavSubmenuItem({list}) {
+    
+
     return (
-        <div>
-            
-        </div>
+        <StyledWrapper>
+            {
+                list.map((data, index) => (
+                    <StyledCol key={index}>
+                        {
+                            data.items instanceof Array
+                            ?
+                            data.items.map((data, index) => (
+                                <NavSubmenuItems key={index} title={data.title} url={data.url} list={data.items || []} />
+                            ))
+                            :
+                            null
+                        }
+                    </StyledCol>
+                ))
+            }
+        </StyledWrapper>
     )
 }
 
 NavSubmenuItem.propTypes = {
-
+    list: PropTypes.array
 }
 
 export default NavSubmenuItem
