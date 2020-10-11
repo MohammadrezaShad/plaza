45529d74255b204ab65de3d44a2672baacb30e20
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-11 11:06:14 +0330

    refactor icons

--- src/components/layouts/main-layout/client/footer/footer-bottom/footer-bottom-links/footer-bottom-links.jsx
+++ src/components/layouts/main-layout/client/footer/footer-bottom/footer-bottom-links/footer-bottom-links.jsx
@@ -1,58 +1,54 @@
-import React, { Fragment } from 'react'
-import { useTranslation } from 'react-i18next'
+import React, { Fragment } from "react";
+import { useTranslation } from "react-i18next";
 
 import {
     Store,
     StoreLink,
-    StoreImage,
-    SocialMedia,
-    SocialMediaLink
-} from './footer-bottom-links.styled'
-import {ReactComponent as Instagram} from '../../../../../../../assets/images/instagram.svg'
-import {ReactComponent as Twitter} from '../../../../../../../assets/images/twitter.svg'
-import {ReactComponent as Facebook} from '../../../../../../../assets/images/facebook.svg'
-import {ReactComponent as Telegram} from '../../../../../../../assets/images/telegram.svg'
-import {ReactComponent as Aparat} from '../../../../../../../assets/images/aparat.svg'
-import GoogleImg from '../../../../../../../assets/images/google.png'
-import BazarImg from '../../../../../../../assets/images/bazar.png'
-import AppleImg from '../../../../../../../assets/images/apple.png'
+    StoreImage, 
+    SocialMedia, 
+    SocialMediaLink,
+    Icon
+ } from "./footer-bottom-links.styled";
+import GoogleImg from "../../../../../../../assets/images/google.png";
+import BazarImg from "../../../../../../../assets/images/bazar.png";
+import AppleImg from "../../../../../../../assets/images/apple.png";
+import IconProvider from "../../../../../../../providers/icon/icon-provider";
 
 const FooterBottomLinks = () => {
-    const { t } = useTranslation()
+  const { t } = useTranslation();
 
+  return (
+    <Fragment>
+      <Store>
+        <StoreLink>
+          <StoreImage src={BazarImg} alt={t("googleAlt")} />
+        </StoreLink>
+        <StoreLink>
+          <StoreImage src={GoogleImg} alt={t("bazarAlt")} />
+        </StoreLink>
+        <StoreLink>
+          <StoreImage src={AppleImg} alt={t("appleAlt")} />
+        </StoreLink>
+      </Store>
+      <SocialMedia>
+        <SocialMediaLink>
+          <Icon as={IconProvider} icon="instagram" size="24px" />
+        </SocialMediaLink>
+        <SocialMediaLink>
+          <Icon as={IconProvider} icon="twitter" size="24px" />
+        </SocialMediaLink>
+        <SocialMediaLink>
+          <Icon as={IconProvider} icon="facebook" size="24px" />
+        </SocialMediaLink>
+        <SocialMediaLink>
+          <Icon as={IconProvider} icon="telegram" size="24px" />
+        </SocialMediaLink>
+        <SocialMediaLink>
+          <Icon as={IconProvider} icon="aparat" size="24px" />
+        </SocialMediaLink>
+      </SocialMedia>
+    </Fragment>
+  );
+};
 
-    return (
-        <Fragment>
-            <Store>
-                <StoreLink>
-                    <StoreImage src={BazarImg} alt={t('googleAlt')} />
-                </StoreLink>
-                <StoreLink>
-                    <StoreImage src={GoogleImg} alt={t('bazarAlt')} />
-                </StoreLink>
-                <StoreLink>
-                    <StoreImage src={AppleImg} alt={t('appleAlt')} />
-                </StoreLink>
-            </Store>
-            <SocialMedia>
-                <SocialMediaLink>
-                    <Instagram />
-                </SocialMediaLink>
-                <SocialMediaLink>
-                    <Twitter />
-                </SocialMediaLink>
-                <SocialMediaLink>
-                    <Facebook />
-                </SocialMediaLink>
-                <SocialMediaLink>
-                    <Telegram />
-                </SocialMediaLink>
-                <SocialMediaLink>
-                    <Aparat />
-                </SocialMediaLink>
-            </SocialMedia>
-        </Fragment>
-    )
-}
-
-export default FooterBottomLinks
+export default FooterBottomLinks;
