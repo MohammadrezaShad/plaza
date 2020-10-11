898ad05173b29e53614cd231810598b71cc42250
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-11 09:33:59 +0330

    add new features

--- src/components/layouts/main-layout/client/header/search-box/search-results/search-results.jsx
+++ src/components/layouts/main-layout/client/header/search-box/search-results/search-results.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 
 import {
     Wrapper,
     Container
 } from './search-results.styled'
-import EmptyResult from './empty-result'
-import LastSearches from './last-searches'
-import MostSearches from './most-searches'
-import ResultsList from './results-list'
+import EmptyResult from "./empty-result";
+import LastSearches from "./last-searches";
+import MostSearches from "./most-searches";
+import ResultsList from "./results-list";
+import MostSearchedTerms from "./most-searched-terms";
 
-const SearchResults = ({results,term}) => {
-    return (
-        <Wrapper>
-            <Container>
-                <ResultsList />
-                <LastSearches />
-                <MostSearches />
-                <EmptyResult />
-            </Container>
-        </Wrapper>
-    )
-}
+const SearchResults = ({ results, term, loading }) => {
+  const renderSwitchCase = () => {
+    if (results.length && term.length >= 2) {
+      return (
+        <Fragment>
+          <ResultsList />
+          <MostSearchedTerms />
+        </Fragment>
+      );
+    } else if (!results.length && term.length >= 2) {
+      return <EmptyResult />;
+    } else {
+      return (
+        <Fragment>
+          <LastSearches />
+          <MostSearches />
+        </Fragment>
+      );
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Container>{renderSwitchCase()}</Container>
+    </Wrapper>
+  );
+};
 
 SearchResults.propTypes = {
+  results: PropTypes.array,
+  term: PropTypes.string,
+  loading: PropTypes.bool,
+};
 
-}
+ 
 
-export default SearchResults
+export default React.memo(SearchResults,areEqual);
