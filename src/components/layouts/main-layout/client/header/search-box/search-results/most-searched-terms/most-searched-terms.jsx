679c86c852a7a950962531132ad44737dfb56fa5
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-11 09:34:42 +0330

    add new features

--- src/components/layouts/main-layout/client/header/search-box/search-results/most-searched-terms/most-searched-terms.jsx
+++ src/components/layouts/main-layout/client/header/search-box/search-results/most-searched-terms/most-searched-terms.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
+import { useTranslation } from "react-i18next";
 import PropTypes from 'prop-types'
 
 import {
-    Wrapper
+    Wrapper,
+    Head,
+    List,
+    Item,
+    ItemLink,
+    Text,
+    Icon
 } from './most-searched-terms.styled'
+import Paths from '../../../../../../../../utils/paths';
+import IconProvider from '../../../../../../../../providers/icon/icon-provider';
 
-const MostSearchedTerms = props => {
+const MostSearchedTerms = ({term}) => {
+    const {t}=useTranslation()
+
     return (
         <Wrapper>
+            <Head>{t("mostSearchedTerms")}{term}</Head>
+            <List>
+                <Item>
+                    <ItemLink to={Paths.home.getPath()}>
+                        <Icon as={IconProvider}  icon="search" size="20px"  />
+                        <Text>باتری اورجینال ال جی</Text>
+                    </ItemLink>
+                </Item>
+                <Item>
+                    <ItemLink to={Paths.home.getPath()}>
+                        <Icon as={IconProvider}   icon="search" size="20px" />
+                        <Text>باتری اورجینال لنوو</Text>
+                    </ItemLink>
+                </Item>
+                <Item>
+                    <ItemLink to={Paths.home.getPath()}>
+                    <Icon as={IconProvider}  icon="search" size="20px"  />
+                        <Text>باتری اورجینال اچ تی سی</Text>
+                    </ItemLink>
+                </Item>
+            </List>
             
         </Wrapper>
     )
