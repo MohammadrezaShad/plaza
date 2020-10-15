a768599a350798cbe86ba8ea2c26a9f557705909
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-15 11:08:59 +0330

    add unauthorized component

---
+++ src/components/shared/unauthorized/unauthorized.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Unauthorized = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Unauthorized.propTypes = {
+
+}
+
+export default Unauthorized
---
+++ src/components/shared/unauthorized/unauthorized.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const StyledWrapper = styled.section`
+    border:1px solid ${({theme}) => theme.palette.stroke}
+`
