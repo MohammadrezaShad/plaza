ba4512b7e7c5ecb8f1278001d317a1c7bdcb1667
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-15 09:08:11 +0330

    refactor

--- src/hoc/with-device-detection.js
+++ src/hoc/with-device-detection.js
@@ -1,29 +1,7 @@
-import React, { Component } from "react";
-import { CheckUserAgent, DeviceTypes } from "../utils/device-detection";
+import React from "react";
+import { CheckUserAgent } from "../utils/device-detection";
 
-
-// const withDeviceDetection = WrappedComponent => {
-//     return class withDeviceDetection extends Component {
-//         constructor(){
-//             super();
-            
-//             console.log(this.props);
-//             const {staticContext} = this.props;
-//             const userAgent = navigator.userAgent;
-//             this.deviceType = CheckUserAgent(userAgent);
-//         }
-
-
-
-//         render() {
-//             return(
-//                 <WrappedComponent {...this.props} deviceType={this.deviceType} /> 
-//             )
-//         }
-//     }
-// }
-
-const withDeviceDetection = WrappedComponent => (props) => {
+const withDeviceDetection = WrappedComponent => props => {
     const {staticContext} = props;
     const userAgent = staticContext && staticContext.req ? staticContext.req.headers['user-agent'] : navigator.userAgent;
     const deviceType = CheckUserAgent(userAgent);
