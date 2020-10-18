da66411d2ea86441e2a98ab8ceb72a903f20c894
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-18 16:06:14 +0330

    fix localization and props

--- src/components/layouts/main-layout/mobile/header/header-top/action-bar/profile/profile-menu/profile-menu.jsx
+++ src/components/layouts/main-layout/mobile/header/header-top/action-bar/profile/profile-menu/profile-menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
 import { 
   StyledWrapper, 
@@ -8,31 +9,34 @@ import {
   StyledIcon 
 } from "./profile-menu.styled";
 import IconProvider from "../../../../../../../../../providers/icon/icon-provider";
-import WithLoginRedux from "../../../../../../../../../hoc/with-login-redux";
+import WithUserState from "../../../../../../../../../hoc/with-user-state";
 import ProfileDetails from "../../../../../../../../shared/profile-details";
 import ProfileMenuItem from "./profile-menu-item";
 import Paths from "../../../../../../../../../utils/paths";
 
 const ProfileMenu = ({ 
-  loggedIn, logoutHandler, 
-  text, 
-  icon, 
-  fillColor, 
-  score, 
+  loggedIn, 
+  firstName, 
+  lastName, 
+  userLevel, 
+  userPoint, 
+  logoutHandler,
   showProfile, 
   toggleProfile }) => {
+
+    const {t}=useTranslation()
 
   return (
     <StyledWrapper showProfile={showProfile}>
       <StyledHeader>
-        <ProfileDetails loggedIn={loggedIn} logoutHandler={logoutHandler} text={text} icon={icon} fillColor={fillColor} score={score} />
+        <ProfileDetails loggedIn={loggedIn} logoutHandler={logoutHandler} firstName={firstName} lastName={lastName} userLevel={userLevel} userPoint={userPoint} />
         <StyledIcon as={IconProvider} icon="close" size="12px" onClick={toggleProfile} />
       </StyledHeader>
       <StyledContent>
-        <ProfileMenuItem text="پروفایل" icon="user" href={Paths.home.getPath()} />
-        <ProfileMenuItem text="باشگاه مشتریان" icon="club" href={Paths.home.getPath()} />
-        <ProfileMenuItem text="سفارش های من" icon="shopping-cart" href={Paths.home.getPath()} />
-        <ProfileMenuItem text="خروج از حساب" icon="logout" href={Paths.home.getPath()} />
+        <ProfileMenuItem text={t('profile')} icon="user" href={Paths.home.getPath()} />
+        <ProfileMenuItem text={t('club')} icon="club" href={Paths.home.getPath()} />
+        <ProfileMenuItem text={t('myOrders')} icon="shopping-cart" href={Paths.home.getPath()} />
+        <ProfileMenuItem text={t('signout')} icon="logout" href={Paths.home.getPath()} />
       </StyledContent>
     </StyledWrapper>
   );
@@ -50,4 +54,4 @@ ProfileMenu.propTypes = {
   toggleProfile: PropTypes.func,
 };
 
-export default WithLoginRedux(ProfileMenu);
+export default WithUserState(ProfileMenu);
