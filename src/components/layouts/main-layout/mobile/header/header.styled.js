672d2edc50a10edd9642c797b5b64ae340adfb86
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-19 15:09:47 +0330

    fix StyledWrapper styles

--- src/components/layouts/main-layout/mobile/header/header.styled.js
+++ src/components/layouts/main-layout/mobile/header/header.styled.js
@@ -4,6 +4,7 @@ export const StyledWrapper = styled.header`
   position: sticky;
   top: 0;
   z-index: ${({ theme }) => theme.zIndex.header};
-  border-bottom:1px solid ${({theme})=>theme.palette.stroke};
+  border-bottom: 1px solid ${({ theme }) => theme.palette.stroke};
+  padding: 0 ${({ theme }) => theme.defaults.screenSideDim};
 `;
 
