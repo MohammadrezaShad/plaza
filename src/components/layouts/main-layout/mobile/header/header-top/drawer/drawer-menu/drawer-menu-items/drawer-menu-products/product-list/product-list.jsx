5c69454f3f82188021ae498f8bbe9f083c0047bc
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-19 11:45:16 +0330

    add React.memo

--- src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/product-list/product-list.jsx
+++ src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/product-list/product-list.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { StyledListWrapper } from "./product-list.styled";
+import { 
+  StyledListWrapper, 
+  StyledContainer 
+} from "./product-list.styled";
 import ProductNestedList from "./product-nested-list";
 
 const ProductList = ({ options, show, depth, root }) => {
   return (
-    <StyledListWrapper $show={show} root={root} depth={depth}>
-      {options.map((option) => (
-        <ProductNestedList key={option.id} option={option} />
-      ))}
-    </StyledListWrapper>
+    <StyledContainer $show={show} root={root}>
+      <StyledListWrapper $show={show} root={root} depth={depth}>
+        {options.map((option) => (
+          <ProductNestedList key={option.id} option={option} />
+        ))}
+      </StyledListWrapper>
+    </StyledContainer>
   );
 };
 
 ProductList.propTypes = {
-  options:PropTypes.array,
-  show:PropTypes.bool,
-  depth:PropTypes.number,
-  root:PropTypes.bool
+  options: PropTypes.array,
+  show: PropTypes.bool,
+  depth: PropTypes.number,
+  root: PropTypes.bool,
 };
 
-export default ProductList;
+export default React.memo(ProductList);
