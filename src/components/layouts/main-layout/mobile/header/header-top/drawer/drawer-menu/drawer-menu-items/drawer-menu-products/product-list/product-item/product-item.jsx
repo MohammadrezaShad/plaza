94ca848fbbbd5ede96d18afa8f8a759018eb13b3
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-19 11:48:06 +0330

    add React.memo

--- src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/product-list/product-item/product-item.jsx
+++ src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/product-list/product-item/product-item.jsx
@@ -11,20 +11,22 @@ import IconProvider from "../../../../../../../../../../../../providers/icon/ico
 const ProductItem = ({ subOptions, label, showToggle, show }) => {
   return (
     <StyledWrapper subOptions={subOptions} onClick={showToggle}>
-      {
-      subOptions ? <StyledIcon $show={show} 
+      {subOptions ? 
+      <StyledIcon 
+      $show={show} 
       as={IconProvider} 
-      icon="chevron-left" size="6px" /> : ""
-      }
+      icon="chevron-left" 
+      size="6px" /> : ""}
       <StyledText $show={show}>{label}</StyledText>
     </StyledWrapper>
   );
 };
+
 ProductItem.propTypes = {
-  show:PropTypes.bool,
-  label:PropTypes.string,
-  showToggle:PropTypes.func,
-  subOptions:PropTypes.number
+  show: PropTypes.bool,
+  label: PropTypes.string,
+  showToggle: PropTypes.func,
+  subOptions: PropTypes.number,
 };
 
-export default ProductItem;
+export default React.memo(ProductItem);
