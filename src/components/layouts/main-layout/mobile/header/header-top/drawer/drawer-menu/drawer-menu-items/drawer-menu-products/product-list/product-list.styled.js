3f34f8de92919bc5d5b05f73c84e6d86ed94c771
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-19 11:45:35 +0330

    add styledContainer

--- src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/product-list/product-list.styled.js
+++ src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/product-list/product-list.styled.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
+
+export const StyledContainer=styled.div`
+   max-height: ${({ $show, root }) => ($show ? "1000px" : root ? "1000px" : "0")};
+  overflow:hidden;
+  margin-bottom: ${({ $show, root, theme }) => root ||!$show ? 0:`-${theme.dim[1]}`}; 
+  transition:${({$show })=>$show ? ' max-height 1s ease-in-out,.1s margin-bottom .2s':'max-height 0.5s cubic-bezier(0, 1, 0, 1),.1s margin-bottom .1s'} ;
+`
+
 export const StyledListWrapper = styled.ul`
-  display: ${({ $show, root }) => ($show ? "block" : root ? "block" : "none")};
-  margin-top: ${({ $show, root, theme }) => (root ? 0 : $show ? theme.dim[1] : 0)};
+  margin-top: ${({ $show, root, theme }) => (root ? 0 :theme.dim[1])};
   background-color: ${({ theme, depth }) => (!depth ? theme.palette.back : depth == 1 ? theme.palette.surface : theme.palette.whisper)};
-  padding: ${({ theme }) => theme.dim[2]};
+  padding: ${({ theme,root }) =>root ? 0: theme.dim[2]};
   &:not(:last-child) {
     margin-bottom: ${({ theme }) => theme.dim[1]};
   }
