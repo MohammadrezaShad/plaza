f79e7fe2eedf97decb0f4600d8d304d833d80c94
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-19 11:44:11 +0330

    fix StyledTitle style

--- src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/drawer-menu-products.styled.js
+++ src/components/layouts/main-layout/mobile/header/header-top/drawer/drawer-menu/drawer-menu-items/drawer-menu-products/drawer-menu-products.styled.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
-export const StyledWrapper = styled.div`
-  max-width: 300px;
-`;
+export const StyledWrapper = styled.div``;
 
 export const StyledTitle = styled.span`
   display: inline-flex;
   align-items: center;
+  margin-bottom:${({theme})=>theme.dim[2]};
   ${({ theme }) => theme.typography.bodyLgBold};
 `;
