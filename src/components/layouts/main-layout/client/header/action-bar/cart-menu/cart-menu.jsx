462d8a903b11362ba045be02c9aaad190f5ae06b
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-19 16:17:47 +0330

    add empty bind

--- src/components/layouts/main-layout/client/header/action-bar/cart-menu/cart-menu.jsx
+++ src/components/layouts/main-layout/client/header/action-bar/cart-menu/cart-menu.jsx
@@ -1,21 +1,20 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import HeaderMenu from '../header-menu'
+import CartMenuEmpty from './cart-menu-empty';
 
 const CartMenu = ({items}) => {
     const length = items instanceof Array ? items.length : 0;
 
     return (
         <HeaderMenu icon="cart-o" count={length} hasHeader={!!length}>
-            <Fragment>
-                {
-                    length > 0
-                    ?
-                    null
-                    :
-                    "empty"
-                }
-            </Fragment>
+            {
+                length > 0
+                ?
+                null
+                :
+                <CartMenuEmpty />
+            }
         </HeaderMenu>
     )
 }
