dba867edeca0f03a50e403d9429cb9385aa849da
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-19 11:00:48 +0330

    add user level icon helper

--- src/components/shared/profile-details/profile-avatar/profile-avatar.jsx
+++ src/components/shared/profile-details/profile-avatar/profile-avatar.jsx
@@ -3,27 +3,13 @@ import PropTypes from "prop-types";
 
 import { StyledAvatar, StyledIcon } from "./profile-avatar.styled";
 import IconProvider from "../../../../providers/icon/icon-provider";
-import userLevel from "../../../../constants/user-levels";
+import getUserLevelIconName from "../../../../helpers/get-user-level-icon-name";
 
 const ProfileAvatar = ({ level }) => {
-  const AvatarSwitch = (level) => {
-    switch (level) {
-      case userLevel.GREEN:
-        return "user";
-      case userLevel.SILVER:
-        return "silver";
-      case userLevel.GOLDEN:
-        return "golden";
-      case userLevel.DIAMOND:
-        return "diamond";
-      default:
-        return "";
-    }
-  };
 
   return (
     <StyledAvatar level={level}>
-      <StyledIcon as={IconProvider} icon={AvatarSwitch(level)} size="32px" />
+      <StyledIcon as={IconProvider} icon={getUserLevelIconName(level)} size="32px" />
     </StyledAvatar>
   );
 };
