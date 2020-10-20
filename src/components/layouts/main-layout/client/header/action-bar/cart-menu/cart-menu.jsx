eab1daa6ca59b15a0574a7042d9b17088a997d13
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-20 11:49:26 +0330

    add header and footer section

--- src/components/layouts/main-layout/client/header/action-bar/cart-menu/cart-menu.jsx
+++ src/components/layouts/main-layout/client/header/action-bar/cart-menu/cart-menu.jsx
@@ -1,17 +1,60 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import HeaderMenu from '../header-menu'
 import CartMenuEmpty from './cart-menu-empty';
 
-const CartMenu = ({items}) => {
+import {
+    StyledWrapper,
+    StyledHeader,
+    StyledTitle,
+    StyledContent,
+    StyledFooter,
+    StyledFooterContent,
+    StyledFooterText,
+    StyledFooterPrice
+} from './cart-menu.styled'
+
+const CartMenu = ({items, totalPrice}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    items=[
+        {
+
+        },
+        {
+
+        }
+    ]
+
     const length = items instanceof Array ? items.length : 0;
 
+    const openHandler = () => {
+        setIsOpen(true);
+    }
+
+
     return (
-        <HeaderMenu icon="cart-o" count={length} hasHeader={!!length}>
+        <HeaderMenu icon="cart-o" count={length} hasHeader={!!length} onOpen={openHandler}>
             {
+                !isOpen
+                ?
+                null:
                 length > 0
                 ?
-                null
+                <StyledWrapper>
+                    <StyledHeader>
+                        <StyledTitle>تعداد {length} کالا در سبد خرید شما</StyledTitle>
+                    </StyledHeader>
+                    <StyledContent>
+
+                    </StyledContent>
+                    <StyledFooter>
+                        <StyledFooterContent>
+                            <StyledFooterText>مبلغ قابل پرداخت</StyledFooterText>
+                            <StyledFooterPrice>{`${(totalPrice || 0).toLocaleString()} ${process.env.REACT_CURRENCY_UNIT}`} </StyledFooterPrice>
+                        </StyledFooterContent>
+                    </StyledFooter>
+                </StyledWrapper>
                 :
                 <CartMenuEmpty />
             }
@@ -20,7 +63,7 @@ const CartMenu = ({items}) => {
 }
 
 CartMenu.propTypes = {
-
+    totalPrice: PropTypes.number
 }
 
-export default CartMenu
+export default React.memo(CartMenu)
