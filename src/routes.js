8e1e9b833f8b9316dc4d8cfa8cbc6e7fbe258ee3
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-21 11:33:50 +0330

    add club route

--- src/routes.js
+++ src/routes.js
@@ -1,45 +1,57 @@
-import MainLayout from './components/layouts/main-layout';
-import Home from './pages';
-import Signin from './pages/signin';
-import ProductDetail from './pages/product';
-import Paths from './utils/paths';
-import Signup from './pages/signup';
-import Profile from './pages/profile'
-import ProfileOrders from './pages/profile/orders'
-
+import MainLayout from "./components/layouts/main-layout";
+import Home from "./pages";
+import Signin from "./pages/signin";
+import ProductDetail from "./pages/product";
+import Paths from "./utils/paths";
+import Signup from "./pages/signup";
+import Profile from "./pages/profile";
+import ProfileOrders from "./pages/profile/orders";
+import ClubLayout from "./components/layouts/club-layout";
+import HomeClub from './pages/club'
 
 const routes = [
-    {
-        path: Paths.signin.getRoute(),
-        component: Signin
-    },
-    {
-        path: Paths.signup.getRoute(),
-        component: Signup
-    },
-    {
-        component: MainLayout,
+  {
+    path: Paths.signin.getRoute(),
+    component: Signin,
+  },
+  {
+    path: Paths.signup.getRoute(),
+    component: Signup,
+  },
+  {
+    component: MainLayout,
+    routes: [
+      {
+        path: Paths.home.getRoute(),
+        exact: true,
+        component: Home,
+      },
+      {
+        path: Paths.product.detail().getRoute(),
+        component: ProductDetail,
+      },
+      {
+        path: Paths.profile.home.getRoute(),
+        exact: true,
+        component: Profile,
+      },
+      {
+        path: Paths.profile.orders.getRoute(),
+        component: ProfileOrders,
+      },
+      {
+        path: Paths.club.home.getRoute(),
+        component: ClubLayout,
         routes: [
-            {
-                path: Paths.home.getRoute(),
-                exact: true,
-                component: Home
-            },
-            {
-                path: Paths.product.detail().getRoute(),
-                component: ProductDetail
-            },
-            {
-                path: Paths.profile.home.getRoute(),
-                exact: true,
-                component: Profile
-            },
-            {
-                path: Paths.profile.orders.getRoute(),
-                component: ProfileOrders
-            }
-        ]
-    }
+          {
+            path: Paths.club.home.getRoute(),
+            exact: true,
+            component: HomeClub,
+          }
+        ],
+      },
+    ],
+  },
 ];
 
 export default routes;
