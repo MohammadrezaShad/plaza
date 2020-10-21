c77d5d3bdc3af1fdd77d58888fe0cd14cb8f42f9
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-21 11:38:56 +0330

    wip mobile title

--- src/components/layouts/main-layout/mobile/main-layout.jsx
+++ src/components/layouts/main-layout/mobile/main-layout.jsx
@@ -14,7 +14,6 @@ const MainLayout = ({ children }) => {
     <Wrapper>
       <Header />
       <Container>
-        <h1>mobile {t("title")}</h1>
         {children}
       </Container>
       <Footer/>
