11a622714023cca36c78626d919c62f739f3df5c
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-21 11:39:28 +0330

    add home view

---
+++ src/components/views/club/home/mobile/home-view.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Title } from "./home-view.styled";
+
+const HomeView = () => {
+    return (
+        <div>
+          <Title>Mobile client</Title>
+        </div>
+      );
+};
+
+export default HomeView;
