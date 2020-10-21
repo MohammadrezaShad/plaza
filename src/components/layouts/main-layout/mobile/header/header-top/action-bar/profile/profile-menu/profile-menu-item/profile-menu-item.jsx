f9c6d3fbfc839a9db192a57637685b5525671264
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-21 08:58:26 +0330

    add logoutHandler

--- src/components/layouts/main-layout/mobile/header/header-top/action-bar/profile/profile-menu/profile-menu-item/profile-menu-item.jsx
+++ src/components/layouts/main-layout/mobile/header/header-top/action-bar/profile/profile-menu/profile-menu-item/profile-menu-item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 import { 
   StyledWrapper, 
@@ -9,10 +10,10 @@ import {
 } from "./profile-menu-item.styled";
 import IconProvider from "../../../../../../../../../../providers/icon/icon-provider";
 
-const ProfileMenuItem = ({ text, icon ,href}) => {
+const ProfileMenuItem = ({ text, icon ,href,logoutHandler}) => {
   return (
     <StyledWrapper>
-      <StyledLink to={href}>
+      <StyledLink as={href ? Link:'div'} to={href} onClick={logoutHandler}>
         <StyledIcon as={IconProvider} icon={icon} size="20px" />
         <StyledText>{text}</StyledText>
       </StyledLink>
@@ -23,7 +24,8 @@ const ProfileMenuItem = ({ text, icon ,href}) => {
 ProfileMenuItem.propTypes = {
   text: PropTypes.string,
   icon: PropTypes.string,
-  href:PropTypes.string
+  href:PropTypes.string,
+  logoutHandler:PropTypes.func
 };
 
 export default ProfileMenuItem;
