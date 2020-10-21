f2111766619e54c7e5da901eb18db42e115e8a9e
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-21 10:47:49 +0330

    add button configs

---
+++ src/constants/button-configs.js
@@ -0,0 +1,18 @@
+export const buttonColors = {
+    PRIMARY: 1,
+    SECONDARY: 2
+}
+
+export const buttonSizes = {
+    LARGE: 1,
+    MEDIUM: 2,
+    S_MEDIUM: 3,
+    SMALL: 4
+}
+
+export const buttonVariants = {
+    FILL: 1,
+    OUTLINE: 2,
+    LINK: 3
+}
+
