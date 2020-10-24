740db0b31cc13499f11b00e150a0711b34496c65
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 08:12:25 +0330

    add getUserLevelName

--- src/helpers/get-user-level-icon-name.js
+++ src/helpers/get-user-level-icon-name.js
@@ -1,18 +1,33 @@
 import userLevel from "../constants/user-levels";
 
 const getUserLevelIconName = (level) => {
-    switch (level) {
-        case userLevel.GREEN:
-          return "user";
-        case userLevel.SILVER:
-          return "silver";
-        case userLevel.GOLDEN:
-          return "golden";
-        case userLevel.DIAMOND:
-          return "diamond";
-        default:
-          return "user";
-      }
-}
+  switch (level) {
+    case userLevel.GREEN:
+      return "user";
+    case userLevel.SILVER:
+      return "silver";
+    case userLevel.GOLDEN:
+      return "golden";
+    case userLevel.DIAMOND:
+      return "diamond";
+    default:
+      return "user";
+  }
+};
 
 export default getUserLevelIconName;
+
+export const getUserLevelName = (level) => {
+  switch (level) {
+    case userLevel.GREEN:
+      return "سبز";
+    case userLevel.SILVER:
+      return "نقره ای";
+    case userLevel.GOLDEN:
+      return "طلایی";
+    case userLevel.DIAMOND:
+      return "الماس";
+    default:
+      return "سبز";
+  }
+};
