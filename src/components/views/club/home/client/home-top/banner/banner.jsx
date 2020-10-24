8bffac86fc2149b71f84cc1b5e0d48c56bcc5f21
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 08:03:01 +0330

    add banner

---
+++ src/components/views/club/home/client/home-top/banner/banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {StyledWrapper} from './banner.styled'
+
+const Banner = ({}) => {
+    return (
+        <div>
+            Banner
+        </div>
+    )
+}
+
+Banner.propTypes = {
+
+}
+
+export default Banner
