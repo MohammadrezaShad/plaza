8398081bb1f13349bb4daff5f62d9c91838bfa61
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 08:59:51 +0330

    fix StyledWrapper styles

--- src/components/views/club/home/client/home-top/home-top.styled.js
+++ src/components/views/club/home/client/home-top/home-top.styled.js
@@ -2,6 +2,5 @@ import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  align-items: stretch;
 `;
