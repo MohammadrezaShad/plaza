b70333e13fc7f3b3798c713aad28fb4942941c1a
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 13:42:14 +0330

    add init content

--- src/components/views/club/home/client/home-top/guide-card/guide-card.styled.js
+++ src/components/views/club/home/client/home-top/guide-card/guide-card.styled.js
@@ -1,1 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  flex: 1 0 25.5%;
+  padding-right:${({theme})=>theme.dim[4]};
+  overflow:hidden;
+`;
+
+export const StyledNumber = styled.span`
+  display:inline-block;
+  text-decoration: underline;
+  color: ${({ theme }) => theme.palette.primary};
+  ${({theme})=>theme.typography.bodyMdBold};
+  margin-left: 5px;
+`;
