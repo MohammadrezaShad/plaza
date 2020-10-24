2ec9c865013be4845a113d90144c16795bf890c8
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 08:03:24 +0330

    add guide-card

---
+++ src/components/views/club/home/client/home-top/guide-card/guide-card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {StyledWrapper} from './guide-card.styled'
+
+const GuideCard = ({}) => {
+    return (
+        <div>
+            GuidCard
+        </div>
+    )
+}
+
+GuideCard.propTypes = {
+
+}
+
+export default GuideCard
