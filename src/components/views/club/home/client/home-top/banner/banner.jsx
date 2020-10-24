91b38f30560318280cb1b31841709925baa0f55b
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 09:00:18 +0330

    add banner content

--- src/components/views/club/home/client/home-top/banner/banner.jsx
+++ src/components/views/club/home/client/home-top/banner/banner.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import {StyledWrapper} from './banner.styled'
+import { 
+    StyledWrapper, 
+    StyledContainer, 
+    StyledImg 
+} from "./banner.styled";
+import BannerImg from "../../../../../../../assets/images/club/banner.png";
 
 const Banner = ({}) => {
-    return (
-        <div>
-            Banner
-        </div>
-    )
-}
+  return (
+    <StyledWrapper>
+        <StyledContainer>
+        <StyledImg src={BannerImg} alt="" />
+        </StyledContainer>
+    </StyledWrapper>
+  );
+};
 
-Banner.propTypes = {
+Banner.propTypes = {};
 
-}
-
-export default Banner
+export default Banner;
--- src/components/views/club/home/client/home-top/banner/banner.styled.js
+++ src/components/views/club/home/client/home-top/banner/banner.styled.js
@@ -1,1 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  width: 50%;
+`;
+
+export const StyledContainer = styled.div`
+  display: block;
+  width: 100%;
+  padding-top: 50%;
+  position: relative;
+  height:100%;
+`;
+
+export const StyledImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
