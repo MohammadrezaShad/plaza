4d81f70c4d184e8aac8e55235747251483123e45
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-24 08:04:43 +0330

    add profile-card-bottom

---
+++ src/components/views/club/home/client/home-top/profile-card/profile-card-bottom/profile-card-bottom.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  padding:0 ${({ theme }) => 
+  theme.dim[4]} ${({ theme }) => theme.dim[2]} ${({ theme }) => theme.dim[4]};
+  max-height:72px;
+`;
