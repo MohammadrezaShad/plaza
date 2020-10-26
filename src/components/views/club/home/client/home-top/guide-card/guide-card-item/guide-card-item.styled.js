63bc626e54c7d955eefe33bfc591eb935d00ba47
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-26 13:29:17 +0330

    fix StyledTitle style

--- src/components/views/club/home/client/home-top/guide-card/guide-card-item/guide-card-item.styled.js
+++ src/components/views/club/home/client/home-top/guide-card/guide-card-item/guide-card-item.styled.js
@@ -20,7 +20,7 @@ export const StyledTitle = styled.span`
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
-  line-height:1;
+  line-height:1.1;
 `;
 
 export const ButtonWrapper = styled.div`
