2af741f56421478dfa102f1adb070a3c8c5e1b1c
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-27 11:42:47 +0330

    add validation component

---
+++ src/components/shared/form/validation/validation.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const lineHeight = 16
+export const StyledWrapper = styled.p`
+    margin-top:2px;
+    ${({theme}) => theme.typography.subtitleXsNormal};
+    color:${({theme}) => theme.palette.error};
+    padding-right:${({theme}) => theme.dim[2]};
+    line-height:${`${lineHeight}px`};
+    max-height:${({show}) => show ? `${lineHeight * 5}px` : 0};
+    transition:.3s all;
+    opacity:${({show}) => show ? 1 : 0};
+
+`
+
+
