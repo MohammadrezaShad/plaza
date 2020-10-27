ce0a41866c9a8afd28e57d8642c3a592a983fedd
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-27 11:55:37 +0330

    add history-view

---
+++ src/components/views/club/history/mobile/history-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {  } from "./history-view.styled";
+
+const HistoryView = () => {
+    return (
+        <div>
+            HistoryMobileClubView
+        </div>
+    )
+}
+
+export default HistoryView
