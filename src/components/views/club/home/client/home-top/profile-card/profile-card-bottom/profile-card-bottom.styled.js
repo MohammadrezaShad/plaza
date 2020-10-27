a3354e9356a6c95ff518d1d57673ccd10ef81c7f
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-27 08:02:37 +0330

    wip move to shared

--- src/components/views/club/home/client/home-top/profile-card/profile-card-bottom/profile-card-bottom.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledWrapper = styled.div`
-  padding:0 ${({ theme }) => 
-  theme.dim[4]} ${({ theme }) => theme.dim[2]} ${({ theme }) => theme.dim[4]};
-`;
--- src/components/views/club/home/client/home-top/profile-card/profile-card-content/profile-card-content.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const StyledWrapper = styled.ul`
-  padding:${({ theme }) => theme.dim[3]} ${({ theme }) => theme.dim[4]};
-`;
