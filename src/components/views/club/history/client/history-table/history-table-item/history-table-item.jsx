53d8579eb4c7ac691fd5943a7455064e96496eda
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-27 16:27:22 +0330

    add content init

--- src/components/views/club/history/client/history-table/history-table-item/history-table-item.jsx
+++ src/components/views/club/history/client/history-table/history-table-item/history-table-item.jsx
@@ -1,32 +1,68 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next'
 
-import { StyledWrapper, StyledContent } from './history-table-item.styled'
+import {
+    StyledWrapper,
+    StyledContent,
+    StyledLink,
+    StyledScore,
+    StyledTitle,
+    StyledSubTitle,
+    StyledStatus,
+    StyledDate,
+} from './history-table-item.styled'
+import Button from '../../../../../../shared/button/button'
+import { buttonColors, buttonSizes, buttonVariants } from '../../../../../../../constants/button-configs'
+import BadgeButton from '../../../../../../shared/badge-button'
 
-const HistoryTableItem = ({ score, pointTitle, pointSubTitle, status, persuasivePoint, detail, href }) => {
+const HistoryTableItem = ({
+    score,
+    pointTitle,
+    pointSubTitle,
+    status,
+    persuasivePoint,
+    detail,
+    href }) => {
+    const { t } = useTranslation()
     return (
         <StyledWrapper>
             <StyledContent>
-                  {score}
+                <StyledScore score={score}>
+                    {score >= 0 ? `+${score}` : score}
+                </StyledScore>
             </StyledContent>
             <StyledContent>
-
+                <StyledTitle>{pointTitle}</StyledTitle>
+                <StyledSubTitle>{pointSubTitle}</StyledSubTitle>
             </StyledContent>
             <StyledContent>
-
+                {persuasivePoint}
             </StyledContent>
             <StyledContent>
-
+                <StyledStatus>
+                    <StyledDate>{status.date}</StyledDate>
+                    <BadgeButton status={status.confirm ? 'success':'check'}>
+                        {status.confirm ? 'تایید امتیاز' : 'در حال بررسی'}
+                    </BadgeButton>
+                </StyledStatus>
             </StyledContent>
             <StyledContent>
-
+                {
+                    detail ?
+                        <StyledLink to={href}>
+                            <Button textWrap={true}  variant={buttonVariants.OUTLINE}>
+                                {t('details')}
+                            </Button>
+                        </StyledLink> : ''
+                }
             </StyledContent>
         </StyledWrapper>
     )
 }
 
 HistoryTableItem.propTypes = {
-    score:PropTypes.number
+    score: PropTypes.number
 }
 
 export default HistoryTableItem
--- src/components/views/club/history/client/history-table/history-table-item/history-table-item.styled.js
+++ src/components/views/club/history/client/history-table/history-table-item/history-table-item.styled.js
@@ -1,14 +1,68 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const StyledWrapper = styled.div`
-  display:flex;
+  display: flex;
   margin: 0 ${({ theme }) => theme.dim[4]};
-  padding:${({theme})=>theme.dim[2]} 0;
-  &:not(:last-child){
-      border-bottom:1px solid ${({theme})=>theme.palette.strokeVariant};
+  padding: ${({ theme }) => theme.dim[2]} 0;
+  &:not(:last-child) {
+    border-bottom: 1px solid ${({ theme }) => theme.palette.strokeVariant};
   }
 `;
 
-export const  StyledContent=styled.div`
+export const StyledContent = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-flow: column;
+  color: ${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.bodyMdNormal};
+  &:not(:first-child) {
+    margin-right: ${({ theme }) => theme.dim[1]};
+  }
+  &:nth-child(1) {
+    flex: 0 0 9%;
+  }
+  &:nth-child(2) {
+    flex: 0 0 23.9%;
+  }
+  &:nth-child(3) {
+    flex: 0 0 14.5%;
+  }
+  &:nth-child(4) {
+    flex: 0 0 30.7%;
+  }
+  &:nth-child(5) {
+    margin-right: auto;
+  }
+`;
 
-`
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const StyledScore = styled.span`
+  display: inline-flex;
+  direction: ltr;
+  justify-content: flex-end;
+  color: ${({ theme, score }) => (score >= 0 ? theme.palette.primary : theme.palette.onSurface)};
+`;
+
+export const StyledTitle = styled.span`
+  display: inline-flex;
+`;
+
+export const StyledSubTitle = styled.span`
+  display: inline-flex;
+  color: ${({ theme }) => theme.palette.subtitle};
+  ${({ theme }) => theme.typography.buttonXsNormal};
+`;
+
+export const StyledStatus = styled.span`
+  display: inline-flex;
+`;
+
+export const StyledDate = styled.span`
+  display: inline-flex;
+  margin-left: ${({ theme }) => theme.dim[1]};
+`;
+
