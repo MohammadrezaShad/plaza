7542fccd11c7ec5429dbbb869e8cb23005bbe687
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-28 10:02:40 +0330

    add StyledTable StyledEmpty

--- src/components/views/club/history/client/history-table/history-table.styled.js
+++ src/components/views/club/history/client/history-table/history-table.styled.js
@@ -2,14 +2,29 @@ import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
   width: 75%;
-  padding:0 ${({theme})=>theme.dim[2]};
+  padding: 0 ${({ theme }) => theme.dim[2]};
 `;
 
-export const StyledContent=styled.div``
+export const StyledContent = styled.div``;
 
-export const StyledContainer=styled.div`
+export const StyledTable=styled.div`
+  display:none;
+`
+
+export const StyledEmpty=styled.div`
+text-align:center;
+`
+
+export const StyledContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content:flex-end;
+  justify-content: flex-end;
   margin-top: ${({ theme }) => theme.dim[2]};
-`
+`;
+
+export const StyledTitle = styled.span`
+  display: inline-flex;
+  ${({ theme }) => theme.typography.headline2}
+  color:${({ theme }) => theme.palette.onSurface};
+  margin-bottom:${({theme})=>theme.dim[6]};
+`;
