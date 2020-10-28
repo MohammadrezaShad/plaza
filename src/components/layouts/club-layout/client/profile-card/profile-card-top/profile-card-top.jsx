461c07ad69e2adf09a90aea12f884ed6621045cb
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-28 16:09:31 +0330

    add profile-card-top

---
+++ src/components/layouts/club-layout/client/profile-card/profile-card-top/profile-card-top.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+
+import { 
+  StyledWrapper, 
+  StlyedDetail, 
+  StyledTitle, 
+  StyledContainer, 
+  StlyedLevel, 
+  StyledLevelText, 
+  StyledLevelValue, 
+  StyledContent 
+} from "./profile-card-top.styled";
+import ProfileAvatar from "../../../../../shared/profile-details/profile-avatar";
+import Button from "../../../../../shared/button";
+import { buttonSizes, buttonVariants } from "../../../../../../constants/button-configs";
+import WithUserState from "../../../../../../hoc/with-user-state";
+import { getUserLevelName } from "../../../../../../helpers/get-user-level-icon-name";
+
+const ProfileCardTop = ({ userLevel, firstName, lastName }) => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledWrapper>
+      <StyledContent>
+        <ProfileAvatar level={userLevel} iconSize='24px' />
+      </StyledContent>
+      <StlyedDetail>
+        <StyledTitle>
+          {firstName} {lastName}
+        </StyledTitle>
+        <StyledContainer>
+          <StlyedLevel>
+            <StyledLevelText>{t("yourLevel")} :   </StyledLevelText>
+            <StyledLevelValue userLevel={userLevel} > {getUserLevelName(userLevel)}</StyledLevelValue>
+          </StlyedLevel>
+          <Button
+          size={buttonSizes.SMALL} 
+          variant={buttonVariants.OUTLINE} 
+          text={t("increaseLevel")} 
+          textWrap={false} />
+        </StyledContainer>
+      </StlyedDetail>
+    </StyledWrapper>
+  );
+};
+
+ProfileCardTop.propTypes = {
+  userLevel: PropTypes.number,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+};
+
+export default WithUserState(ProfileCardTop);
