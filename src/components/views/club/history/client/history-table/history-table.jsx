05f19a44ba898bedc34974a88949f9bcd3d6ebcd
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-28 10:01:58 +0330

    add HistoryEmpty component

--- src/components/views/club/history/client/history-table/history-table.jsx
+++ src/components/views/club/history/client/history-table/history-table.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
 
-import { StyledWrapper, StyledContent, StyledContainer } from './history-table.styled'
+import { StyledWrapper, StyledContent, StyledContainer, StyledTable, StyledEmpty, StyledTitle } from './history-table.styled'
 import HistoryTableHead from './history-table-head'
 import HistoryTableItem from './history-table-item'
 import Pagination from '../../../../../shared/pagination'
 import Paths from '../../../../../../utils/paths'
+import HistoryEmpty from './history-empty'
 
 
 
@@ -14,35 +15,44 @@ const HistoryTable = props => {
     const { t } = useTranslation()
     return (
         <StyledWrapper>
-            <HistoryTableHead
-                items={
-                    [t('score'),
-                    t('pointTitle'),
-                    t('persuasivePoint'),
-                    t('status'),
-                    t('blank')]
-                } />
-            <StyledContent>
-                <HistoryTableItem
-                    score={1000}
-                    pointTitle="دعوت دوستان"
-                    pointSubTitle="ahmad.n33@gmail.com"
-                    status={{ confirm: true, date: "‏1 فروردین 1399" }}
-                    persuasivePoint="ندارد"
-                    detail={true}
-                    href={Paths.home.getRoute()} />
-                <HistoryTableItem
-                    score={-1000}
-                    pointTitle="دعوت دوستان"
-                    pointSubTitle="ahmad.n33@gmail.com"
-                    status={{ confirm: false, date: "‏1 فروردین 1399" }}
-                    persuasivePoint="ندارد"
-                    detail={true}
-                    href={Paths.home.getRoute()} />
-            </StyledContent>
-            <StyledContainer>
-                <Pagination />
-            </StyledContainer>
+            <StyledTitle>{t('plazaClubArea.historyOfPointGraph')}</StyledTitle>
+            <StyledEmpty>
+                <HistoryEmpty />
+            </StyledEmpty>
+            <StyledTable>
+                <HistoryTableHead
+                    items={
+                        [t('score'),
+                        t('pointTitle'),
+                        t('persuasivePoint'),
+                        t('status'),
+                        t('blank')]
+                    } />
+                <StyledContent>
+                    <HistoryTableItem
+                        score={1000}
+                        pointTitle="دعوت دوستان"
+                        pointSubTitle="ahmad.n33@gmail.com"
+                        status={{ confirm: true, date: "‏1 فروردین 1399" }}
+                        persuasivePoint="ندارد"
+                        detail={true}
+                        href={Paths.home.getRoute()} />
+                    <HistoryTableItem
+                        score={-1000}
+                        pointTitle="دعوت دوستان"
+                        pointSubTitle="ahmad.n33@gmail.com"
+                        status={{ confirm: false, date: "‏1 فروردین 1399" }}
+                        persuasivePoint="ندارد"
+                        detail={true}
+                        href={Paths.home.getRoute()} />
+                </StyledContent>
+                <StyledContainer>
+                    <Pagination />
+                </StyledContainer>
+            </StyledTable>
+            <StyledEmpty>
+
+            </StyledEmpty>
         </StyledWrapper>
     )
 }
