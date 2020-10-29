62ebde588206ebf7c911a7c1f93c85a2bf9fb1fc
Siyamak_Plaza <habibipour@gilertebat.com>
2020-10-29 10:50:07 +0330

    fix score chart button link

--- src/components/layouts/club-layout/client/profile-card/profile-card-content/profile-card-content.jsx
+++ src/components/layouts/club-layout/client/profile-card/profile-card-content/profile-card-content.jsx
@@ -11,7 +11,7 @@ const ProfileCardContent = ({}) => {
   return (
     <StyledWrapper>
       <CardItem text={t("plazaClubArea.dashboard")} href={Paths.club.home.getPath()} icon="hamburger-menu" />
-      <CardItem text={t("plazaClubArea.pointGraph")} href={Paths.signin.getPath()} icon="statistics" />
+      <CardItem text={t("plazaClubArea.pointGraph")} href={Paths.club.score.getPath()} icon="statistics" />
       <CardItem text={t("plazaClubArea.historyOfPointGraph")} href={Paths.club.history.getPath()} icon="history" />
       <CardItem text={t("plazaClubArea.buyTogether")} href={Paths.about.getPath()} icon="user" />
     </StyledWrapper>
