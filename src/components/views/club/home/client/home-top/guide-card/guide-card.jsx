40518c34977179e42a6f25da46daa8f6fe6c9d35
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-10-31 08:31:16 +0330

    add BuyTogether component

--- src/components/views/club/home/client/home-top/guide-card/guide-card.jsx
+++ src/components/views/club/home/client/home-top/guide-card/guide-card.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Trans, useTranslation } from "react-i18next";
 
 import { StyledWrapper, StyledNumber } from "./guide-card.styled";
 import GuideCardItem from "./guide-card-item";
+import BuyTogether from "../../../../../../dialogs/club/buy-together";
 
 const GuideCard = ({ point = 10000, nextLevelPoint = 15000 }) => {
   const { t } = useTranslation();
-
+  const [showModal, setshowModal] = useState(false)
+  const btnClick = () => {
+    setshowModal(!showModal)
+  }
   return (
     <StyledWrapper>
-      <GuideCardItem 
-      title={t("plazaClubArea.guideTitleOne")} 
-      buttonText={t("increaseLevel")} 
-      text={t("plazaClubArea.guideTextOne", { point: "2550" })} >
-        فقط <StyledNumber>{nextLevelPoint-point}</StyledNumber>
+      <GuideCardItem
+        title={t("plazaClubArea.guideTitleOne")}
+        buttonText={t("increaseLevel")}
+        text={t("plazaClubArea.guideTextOne", { point: "2550" })} >
+        فقط <StyledNumber>{nextLevelPoint - point}</StyledNumber>
       </GuideCardItem>
 
-      <GuideCardItem 
-      title={t("plazaClubArea.guideTitleTwo")} 
-      buttonText={t("plazaClubArea.addNewBuyTogether")} 
-      text={t("plazaClubArea.guideTextTwo")} 
-      buttonColor={true} />
-      
+      <GuideCardItem
+        title={t("plazaClubArea.guideTitleTwo")}
+        buttonText={t("plazaClubArea.addNewBuyTogether")}
+        text={t("plazaClubArea.guideTextTwo")}
+        buttonColor={true}
+        btnClick={btnClick}
+      />
+      <BuyTogether open={showModal||false} onClose={setshowModal} />
     </StyledWrapper>
   );
 };
