8ca28fd3baf983dd7bf3b451295dba171dc870a1
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-01 15:32:52 +0330

    wipe content

--- src/components/shared/product/product-item/product-item-detail/product-item-detail.jsx
+++ src/components/shared/product/product-item/product-item-detail/product-item-detail.jsx
@@ -4,33 +4,19 @@ import { useTranslation } from 'react-i18next'
 
 import {
     StyledWrapper,
-    StyledDetail,
     StyledContent,
-    StyledLink,
-    StyledPoint,
     StyledPrice,
     StyledOffPrice,
     StyledCurrency
 } from './product-item-detail.styled'
-import Button from '../../../button/button'
-import { buttonSizes } from '../../../../../constants/button-configs'
 
-const ProductItemDetail = ({ price, offPrice, href, point, specialOffer }) => {
+const ProductItemDetail = ({ price, offPrice }) => {
     const { t } = useTranslation()
 
     return (
         <StyledWrapper>
-            {
-                specialOffer ?
-                    <StyledDetail>
-                        <StyledPoint>{t('plazaClubArea.buyScore')}: {point}</StyledPoint>
-                        <StyledLink to={href}>
-                            <Button text={t('details')} size={buttonSizes.MEDIUM} textWrap={false} />
-                        </StyledLink>
-                    </StyledDetail> : ''
-            }
             <StyledContent>
-                <StyledPrice specialOffer={specialOffer}>{price.toLocaleString()}</StyledPrice>
+                <StyledPrice >{price.toLocaleString()}</StyledPrice>
                 <StyledOffPrice>{offPrice.toLocaleString()}</StyledOffPrice>
                 <StyledCurrency>{t('currency')}</StyledCurrency>
             </StyledContent>
@@ -41,9 +27,6 @@ const ProductItemDetail = ({ price, offPrice, href, point, specialOffer }) => {
 ProductItemDetail.propTypes = {
     price: PropTypes.number,
     offPrice: PropTypes.number,
-    href: PropTypes.string,
-    point: PropTypes.number,
-    specialOffer:PropTypes.bool
 }
 
 export default ProductItemDetail
