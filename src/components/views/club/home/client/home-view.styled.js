05a9ec9153c50098a85c56f58549c1b1e6eda6a0
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-01 09:32:18 +0330

    wipe StyledLayout

--- src/components/views/club/home/client/home-view.styled.js
+++ src/components/views/club/home/client/home-view.styled.js
@@ -1,11 +1,4 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
-  padding: ${({ theme }) => theme.dim[4]} ${({ theme }) => theme.defaults.screenSideDim};
-`;
-
-export const StyledLayout = styled.section`
-  max-width: 1600px;
-  margin-right: auto;
-  margin-left: auto;
 `;
