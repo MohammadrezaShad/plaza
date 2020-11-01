fa4fb578a7d9873e8111fdcdb6fc3a511939fd16
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-01 15:33:50 +0330

    fix StyledWrapper and StyledTitle styles

--- src/components/views/club/home/client/home-bottom/home-bottom.styled.js
+++ src/components/views/club/home/client/home-bottom/home-bottom.styled.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
-  margin-top: ${({ theme }) => theme.dim[4]};
+  margin-top: ${({ theme }) => theme.dim[3]};
+  padding:${({theme})=>theme.dim[4]};
+  border:1px solid ${({theme})=>theme.palette.stroke};
 `;
 
 export const StyledTitle = styled.span` 
-  display:inline-block;
+  display:block;
   ${({theme})=>theme.typography.headline2}
   color: ${({ theme }) => theme.palette.onSurface};
+  padding-bottom:${({theme})=>theme.dim[2]};
   margin-bottom:${({theme})=>theme.dim[2]};
+  border-bottom:1px solid ${({theme})=>theme.palette.strokeVariant};
 `;
