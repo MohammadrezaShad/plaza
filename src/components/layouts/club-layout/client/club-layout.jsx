8824eb203bcffad076c4ccac187c83410df0f5cc
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-01 09:30:15 +0330

    add CardWrapper MainWrapper Layout

--- src/components/layouts/club-layout/client/club-layout.jsx
+++ src/components/layouts/club-layout/client/club-layout.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Wrapper, Container } from "./club-layout.styled";
+import { 
+  Wrapper, 
+  Container, 
+  CardWrapper, 
+  MainWrapper, 
+  Layout 
+} from "./club-layout.styled";
 import Header from "./header";
+import ProfileCard from "./profile-card";
 
 const ClubLayout = ({ children }) => {
   return (
     <Wrapper>
       <Header />
-      <Container>{children}</Container>
+      <Container>
+        <Layout>
+          <CardWrapper>
+            <ProfileCard />
+          </CardWrapper>
+          <MainWrapper>
+            {children}
+          </MainWrapper>
+        </Layout>
+      </Container>
     </Wrapper>
   );
 };
--- src/components/layouts/club-layout/client/club-layout.styled.js
+++ src/components/layouts/club-layout/client/club-layout.styled.js
@@ -1,18 +1,35 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    min-height:100%;
-    display:flex;
-    flex-direction:column;
-    direction:${({theme}) => theme.defaults.direction};
-    
-`
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  direction: ${({ theme }) => theme.defaults.direction};
+`;
 
 const Container = styled.div`
-    flex:1;
+  flex: 1;
+  padding: ${({ theme }) => theme.dim[4]} ${({ theme }) => theme.defaults.screenSideDim};
+`;
+
+const Layout=styled.div`
+  display: flex;
+  max-width: 1600px;
+  margin: 0 auto;
 `
 
-export {
-    Wrapper,
-    Container
-}
+ const CardWrapper = styled.div`
+  align-self: flex-start;
+  margin-left: ${({ theme }) => theme.dim[4]};
+  flex: 0 0 23.5%;
+`;
+
+ const MainWrapper = styled.div``;
+
+export { 
+    Wrapper, 
+    Container, 
+    CardWrapper, 
+    MainWrapper,
+    Layout 
+};
