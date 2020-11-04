266612d2e982ec9a3c98da423727bf9fdd0d1399
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-04 16:04:11 +0330

    add buy-together-view

---
+++ src/components/views/club/buy-together/mobile/buy-together-view.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+
+const BuyTogetherView = () => {
+    return (
+        <Fragment>
+            BuyTogetherViewClientMobile
+        </Fragment>
+    );
+};
+
+export default BuyTogetherView;
