6163c3dc39026f92b3408ff8d79c60f42b54c740
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-05 12:50:09 +0330

    add init content

--- src/components/views/club/score/client/score-progress/score-porgress.jsx
+++ src/components/views/club/score/client/score-progress/score-porgress.jsx
@@ -1,16 +1,72 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next'
 
-const ScorePorgress = props => {
+import {
+    StyledWrapper,
+    StyledProgress,
+    StyledTitle,
+    StyledLevel,
+    StyledNumber,
+    StyledTooltipNumber,
+    StyledProgressGreenLevel,
+    StyledProgressSilverLevel,
+    StyledProgressGoldLevel,
+    StyledProgressDiamondLevel,
+    StyledTooltip
+} from './score-progress.styled'
+import IconProvider from '../../../../../../providers/icon/icon-provider'
+
+const ScorePorgress = ({ point = 2410, maxPoint = 25000 }) => {
+    const { t } = useTranslation()
     return (
-        <div>
-            score progress
-        </div>
+        <StyledWrapper>
+            <StyledTitle>
+                {t("plazaClubArea.increaseLevelBar")}
+            </StyledTitle>
+            <StyledProgress progress={point / maxPoint}>
+                <StyledProgressGreenLevel>
+                    <StyledNumber>
+                        {t("plazaClubArea.greenLevel").toLocaleString()}
+                    </StyledNumber>
+                </StyledProgressGreenLevel>
+                <StyledProgressSilverLevel>
+                    <StyledNumber>
+                        {t("plazaClubArea.silverLevel").toLocaleString()}
+                    </StyledNumber>
+                    <StyledLevel>
+                        {t("plazaClubArea.increaseSilverLevelBar")}
+                    </StyledLevel>
+                </StyledProgressSilverLevel>
+                <StyledProgressGoldLevel>
+                    <StyledNumber>
+                        {t("plazaClubArea.goldLevel").toLocaleString()}
+                    </StyledNumber>
+                    <StyledLevel>
+                        {t("plazaClubArea.increaseGoldLevelBar")}
+                    </StyledLevel>
+                </StyledProgressGoldLevel>
+                <StyledProgressDiamondLevel>
+                    <StyledNumber>
+                        {t("plazaClubArea.diamondLevel").toLocaleString()}
+                    </StyledNumber>
+                    <StyledLevel>
+                        <IconProvider icon="diamond" size="16px" />
+                    </StyledLevel>
+                </StyledProgressDiamondLevel>
+                <StyledTooltip progress={point / maxPoint}>
+                    <StyledTooltipNumber>
+                        {point.toLocaleString()}
+                    </StyledTooltipNumber>
+                </StyledTooltip>
+            </StyledProgress>
+        </StyledWrapper>
     )
 }
 
 ScorePorgress.propTypes = {
-
+    point: PropTypes.number,
+    maxPoint: PropTypes.number
 }
 
 export default ScorePorgress
--- src/components/views/club/score/client/score-progress/score-progress.styled.js
+++ src/components/views/club/score/client/score-progress/score-progress.styled.js
@@ -1,1 +1,133 @@
-import moduleName from 'styled-components'
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  padding: ${({ theme }) => theme.dim[3]} ${({ theme }) => theme.dim[4]};
+  border: 1px solid ${({ theme }) => theme.palette.stroke};
+`;
+
+export const StyledTitle = styled.span`
+  display: inline-flex;
+  color: ${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.headline2};
+  margin-bottom: ${({ theme }) => theme.dim[5]};
+`;
+
+export const StyledContent = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: row-reverse;
+`;
+
+export const StyledProgress = styled.div`
+  display: flex;
+  height: 10px;
+  border-radius: 5px;
+  background-color: transparent;
+  flex-flow: row-reverse;
+  position: relative;
+  margin-top: ${({ theme }) => theme.dim[3]};
+  margin-bottom:${({ theme }) => theme.dim[6]};
+  background-image: ${({ theme }) => `linear-gradient(to right, ${theme.palette.surface} 85.3%,${theme.palette.stroke} 85.4%,${theme.palette.stroke} 92.3%,${theme.palette.disabled} 92.4%,${theme.palette.disabled} 100%)`};
+  &:after {
+    content: "";
+    position: absolute;
+    border-radius: 5px;
+    height: 10px;
+    left: 0;
+    width: ${({ progress }) => `${progress * 100}%`};
+    background-color: ${({ theme }) => theme.palette.success};
+  }
+`;
+
+export const StyledNumber=styled.span`
+    position: absolute;
+    display: inline-block;
+    color: ${({ theme }) => theme.palette.subtitle};
+    left: 0;
+    transform:translateX(-100%);
+    top: -16px;
+    ${({ theme }) => theme.typography.subtitleXsNormal};
+`
+
+
+export const StyledLevel=styled(StyledNumber)`
+  top:auto;
+  bottom:-24px;
+  white-space:nowrap;
+`
+
+export const StyledProgressGreenLevel = styled.div`
+  flex: 0 0 85.8%;
+  border-radius: 5px;
+  border-right: 20px solid ${({ theme }) => theme.palette.surface};
+  position: relative;
+  ${StyledNumber}{
+    transform:translateX(0);
+  }
+`;
+
+export const StyledProgressSilverLevel = styled.div`
+  flex: 0 0 7%;
+  border-radius: 5px;
+  border-right: 20px solid ${({ theme }) => theme.palette.stroke};
+  position: relative;
+`;
+
+export const StyledProgressGoldLevel = styled.div`
+  flex:1;
+  border-radius: 5px;
+  border-right: 20px solid ${({ theme }) => theme.palette.disabled};
+  position: relative;
+`;
+
+export const StyledProgressDiamondLevel=styled.div`
+  flex: 0 0 0;
+  position: relative;
+
+`
+
+export const StyledTooltip = styled.span`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  color: ${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.bodyMdNormal};
+  top: -42px;
+  left: ${({ progress }) => `${progress * 100}%`};
+  transform: translateX(-50%);
+  width: 72px;
+  height: 31px;
+  border: 1px solid ${({ theme }) => theme.palette.stroke};
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: -7px;
+    right: 50%;
+    transform: translateX(50%);
+    width: 0px;
+    height: 0px;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-top: 7px solid ${({ theme }) => theme.palette.back};
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    bottom: -8px;
+    right: 50%;
+    transform: translateX(50%);
+    width: 0px;
+    height: 0px;
+    border-left: 8.5px solid transparent;
+    border-right: 8.5px solid transparent;
+    border-top: 8px solid ${({ theme }) => theme.palette.stroke};
+  }
+`;
+
+export const StyledTooltipNumber=styled.span`
+ display:inline-flex;
+ line-height:1;
+ transform:translateY(20%);
+`
+
