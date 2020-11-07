6388c8ec7aa1f713e620e13cebd2835b0ad44a21
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-07 10:41:59 +0330

    fix StyledWrapper style

--- src/components/shared/product/product.styled.js
+++ src/components/shared/product/product.styled.js
@@ -6,23 +6,25 @@ export const StyledWrapper = styled.div`
   overflow: hidden;
   position: relative;
   width: 100%;
-  & > button{
-      position:absolute;
-      transform:translateY(-50%);
-      top:50%;
-      left:0;
-      z-index:10;
-      &:first-child{
-          left:auto;
-          right:0;
-      }
+  & > button {
+    position: absolute;
+    transform: translateY(-50%);
+    top: 50%;
+    left: 0;
+    z-index: 10;
+    width: 30px;
+    background-color: ${({ theme }) => theme.palette.back};
+    &:first-child {
+      left: auto;
+      right: 0;
+    }
   }
 `;
 
 export const StyledContent = styled.div`
   position: relative;
   transition: all 500ms ease 0s;
-  left: ${({$left})=>`${$left}%`};
+  left: ${({ $left }) => `${$left}%`};
   right: auto;
   top: auto;
 `;
@@ -31,9 +33,9 @@ export const StyledContainer = styled.ul`
   display: flex;
 `;
 
-export const StyledProducts=styled.div``
+export const StyledProducts = styled.div``;
 
-export const StyledIcon=styled(IconProvider)`
- position:absolute;
- top:0;
-`
+export const StyledIcon = styled(IconProvider)`
+  position: absolute;
+  top: 0;
+`;
