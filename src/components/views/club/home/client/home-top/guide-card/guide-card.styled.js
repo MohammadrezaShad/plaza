4f426603be9ba89a2a1c6f66afbd43b54797b0e7
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-07 16:24:51 +0330

    add StyledContent

--- src/components/views/club/home/client/home-top/guide-card/guide-card.styled.js
+++ src/components/views/club/home/client/home-top/guide-card/guide-card.styled.js
@@ -4,14 +4,22 @@ export const StyledWrapper = styled.div`
   display: flex;
   flex-flow: column;
   flex: 0 0 31.5%;
-  min-width:0;
+  min-width: 0;
   margin-right: ${({ theme }) => theme.dim[4]};
 `;
 
 export const StyledNumber = styled.span`
-  display:inline-block;
+  display: inline-block;
   text-decoration: underline;
   color: ${({ theme }) => theme.palette.primary};
-  ${({theme})=>theme.typography.bodyMdBold};
+  ${({ theme }) => theme.typography.bodyMdBold};
   margin-left: 5px;
+`;
+
+export const StyledContent = styled.div`
+  display: ${({ $display }) => ($display ? "none" : "block")};
+  margin-top: ${({ theme }) => theme.dim[2]};
+  ${({ theme }) => theme.breakPoints.desktop} {
+    display: ${({ $display }) => ($display ? "block" : "none")};
+  }
 `;
