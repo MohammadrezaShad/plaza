fed902cceff518016e7f923d6a21955f92ec8a5f
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-07 09:11:01 +0330

    add content

--- src/components/views/club/home/client/home-top/banner/banner.jsx
+++ src/components/views/club/home/client/home-top/banner/banner.jsx
@@ -2,20 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 
-import { 
-    StyledWrapper, 
-    StyledContainer, 
-    StyledImg 
+import {
+  StyledWrapper,
+  StyledContainer,
+  StyledImg,
+  StyledHashtag,
+  StyledContent,
+  StyledText,
+  StyledIcon,
+  StyledDetail,
+  StyledSubText,
+  StyledHashtagIcon,
+  StyledHashtagText
 } from "./banner.styled";
-import BannerImg from "../../../../../../../assets/images/club/banner.png";
+import BannerImg from "../../../../../../../assets/images/club/bannerBg.png";
+import IconProvider from "../../../../../../../providers/icon/icon-provider";
 
-const Banner = ({}) => {
+const Banner = ({ }) => {
   const { t } = useTranslation();
 
   return (
     <StyledWrapper>
       <StyledContainer>
         <StyledImg src={BannerImg} alt={t("clubBannerAlt")} />
+        <StyledHashtag>
+          <StyledHashtagIcon as={IconProvider} icon="instagram" size="16px" />
+          <StyledHashtagText>{t("plazaClubArea.profitHashtag")}</StyledHashtagText>
+        </StyledHashtag>
+        <StyledContent>
+          <StyledIcon as={IconProvider} icon="club" size="85px" />
+          <StyledDetail>
+            <StyledText>{t("plazaClubArea.profit")}</StyledText>
+            <StyledSubText>{t("plazaClubArea.clubSLogan")}</StyledSubText>
+          </StyledDetail>
+        </StyledContent>
       </StyledContainer>
     </StyledWrapper>
   );
--- src/components/views/club/home/client/home-top/banner/banner.styled.js
+++ src/components/views/club/home/client/home-top/banner/banner.styled.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import BannerGraphicImg from "../../../../../../../assets/images/club/bannerGraphic.png";
 
 export const StyledWrapper = styled.div`
-  flex:1;
+  flex: 1;
 `;
 
 export const StyledContainer = styled.div`
@@ -9,13 +10,64 @@ export const StyledContainer = styled.div`
   width: 100%;
   padding-top: 50%;
   position: relative;
-  height:100%;
+  height: 100%;
+  background-image:url(${BannerGraphicImg});
+  background-repeat:no-repeat;
+  background-position:right bottom;
 `;
 
 export const StyledImg = styled.img`
   position: absolute;
+  z-index:-1;
+  object-fit:cover;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
+`;
+
+export const StyledHashtag = styled.span`
+  display: inline-flex;
+  align-items: center;
+  position: absolute;
+  right: 4%;
+  bottom: 7.8%;
+`;
+
+export const StyledHashtagIcon = styled.span`
+  color: ${({ theme }) => theme.palette.onSurface};
+`;
+export const StyledHashtagText = styled.span`
+  display: inline-flex;
+  color: ${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.subtitleXsNormal};
+  margin-right: 4px;
+`;
+export const StyledContent = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  right: 4%;
+  top: 7.8%;
+`;
+export const StyledDetail = styled.div`
+  display: flex;
+  flex-flow: column;
+  max-width: 289px;
+  margin-right: ${({ theme }) => theme.dim[4]};
+`;
+export const StyledText = styled.span`
+  display: inline-flex;
+  color: ${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.body2XLgBold};
+`;
+
+export const StyledSubText = styled.span`
+  display: inline-flex;
+  color: ${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.bodyMdNormal};
+`;
+
+export const StyledIcon = styled.span`
+  color: ${({ theme }) => theme.palette.onSurface};
 `;
