0409cb371a8bedb20a7918fd7084de62d9403e41
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-08 09:37:08 +0330

    add signup-view

---
+++ src/components/views/signup/mobile/signup-view.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SignupView = props => {
+    return (
+        <div>
+            SignUpViewMobile
+        </div>
+    )
+}
+
+SignupView.propTypes = {
+
+}
+
+export default SignupView
