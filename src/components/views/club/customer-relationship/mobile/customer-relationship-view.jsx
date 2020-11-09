3675cc596342c68d22b0271ded5ca48e46819d63
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-09 11:19:50 +0330

    add customer-relationship-view

---
+++ src/components/views/club/customer-relationship/mobile/customer-relationship-view.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+
+const CustomerRelationshipView = () => {
+    return (
+        <Fragment>
+            BuyTogetherViewClientMobile
+        </Fragment>
+    );
+};
+
+export default CustomerRelationshipView;
