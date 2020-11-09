0a3f9793e87410169c6b20bdf631cde25e44731b
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-09 16:07:42 +0330

    add StyledHeader

--- src/components/shared/tooltip/tooltip.jsx
+++ src/components/shared/tooltip/tooltip.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { StyledTooltip, StyledTooltipContent } from './tooltip.styled'
+import { StyledTooltip, StyledHeader } from './tooltip.styled'
 
-const Tooltip = ({ text,left,top,children}) => {
+const Tooltip = ({ text, left, top, children,title }) => {
     return (
         <StyledTooltip $left={left} $top={top}>
-                     {text}
-                     {children}
+            {
+              title ?   <StyledHeader>{title}</StyledHeader>:null
+            }
+                {text}
+                {children}
         </StyledTooltip>
     )
 }
 
 Tooltip.propTypes = {
     text: PropTypes.string,
-    left:PropTypes.string,
-    top:PropTypes.string,
-    children:PropTypes.node
+    title: PropTypes.string,
+    left: PropTypes.string,
+    top: PropTypes.string,
+    children: PropTypes.node
 }
 
 export default Tooltip
--- src/components/shared/tooltip/tooltip.styled.js
+++ src/components/shared/tooltip/tooltip.styled.js
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 
-export const StyledTooltip = styled.span`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
+export const StyledTooltip = styled.div`
   position: absolute;
-
   top: ${({ $top }) => ($top ? $top : 0)};
   left: ${({ $left }) => ($left ? $left : 0)};
   transform: translateX(-50%);
-  padding: ${({ theme }) => theme.dim[1]} ${({ theme }) => theme.dim[2]};
   border: 1px solid ${({ theme }) => theme.palette.stroke};
+  border-radius: ${({ theme }) => theme.defaults.borderRadius};
+  background-color: ${({ theme }) => theme.palette.back};
+  
   &:after {
     content: "";
     position: absolute;
@@ -37,3 +35,11 @@ export const StyledTooltip = styled.span`
   }
 `;
 
+export const StyledHeader = styled.span`
+  display: flex;
+  ${({theme})=>theme.typography.headline2};
+  padding: ${({ theme }) => theme.dim[1]} ${({ theme }) => theme.dim[2]};
+  background-color: ${({ theme }) => theme.palette.surface};
+  border-top-right-radius: ${({ theme }) => theme.defaults.borderRadius};
+  border-top-left-radius: ${({ theme }) => theme.defaults.borderRadius};
+`;
