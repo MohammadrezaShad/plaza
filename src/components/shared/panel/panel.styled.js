4630c513b8f1e6e7bec06abeaa6acada6d4b7a17
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-10 11:49:34 +0330

    wip

--- src/components/shared/panel/panel.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledWrapper = styled.div`
-  border: 1px solid ${({ theme }) => theme.palette.stroke};
-  border-radius: ${({ theme }) => theme.defaults.borderRadius};
-`;
