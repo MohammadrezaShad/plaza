42cbae10a80718140a470b7274fa99b0d3b5b24f
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-14 16:18:59 +0330

    add type error success prop

--- src/components/shared/form/textbox/textbox.jsx
+++ src/components/shared/form/textbox/textbox.jsx
@@ -11,30 +11,60 @@ import IconProvider from '../../../../providers/icon/icon-provider';
 const Textbox = ({
     autocomplete,
     placeholder,
-    icon, 
-    showIcon, 
-    buttonCallback, 
+    icon,
+    showIcon = true,
+    type,
+    error,
+    success,
+    buttonCallback,
     onChange,
-    inputValue,
     reference,
     ...props
 }) => {
     const [iconIsShow, setIconIsShow] = useState(showIcon || !!icon);
-
+    const [inputIcon, setInputIcon] = useState('eye-open')
+    const [inputType, setInputType] = useState(type || 'text')
+    const toggleType = () => {
+        if (inputType === 'password') {
+            setInputType('text')
+            setInputIcon('close')
+        } else {
+            setInputType('password')
+            setInputIcon('eye-open')
+        }
+    }
+    const renderIcon = () => {
+        if (type === "password") {
+            return (
+                <Icon text={inputIcon} onClick={toggleType}>
+                    <IconProvider icon={inputIcon} size="16px" />
+                </Icon>
+            )
+        } else if (iconIsShow) {
+            return (
+                <Icon as={buttonCallback ? 'button' : 'span'} onClick={buttonCallback} buttonCallback={buttonCallback}>
+                    {icon ? <IconProvider icon={icon} size="16px" /> : null}
+                </Icon>
+            )
+        }
+    }
     return (
         <Wrapper>
             {
-                iconIsShow ? <Icon as={buttonCallback ? 'button' : 'span'} onClick={buttonCallback}><IconProvider icon={icon} size="16px" /></Icon> : null
+                renderIcon()
             }
-            <Input 
-                {...props} 
-                spellCheck={false} 
-                placeholder={placeholder} 
-                hasIcon={iconIsShow} 
-                onChange={onChange} 
-                ref={reference} 
+            <Input
+                {...props}
+                error={error}
+                success={success}
+                type={inputType}
+                spellCheck={false}
+                placeholder={placeholder}
+                hasIcon={iconIsShow}
+                onChange={onChange}
+                ref={reference}
                 autoComplete={autocomplete}
-                />
+            />
         </Wrapper>
     )
 }
@@ -44,7 +74,10 @@ Textbox.propTypes = {
     icon: PropTypes.string,
     showIcon: PropTypes.bool,
     onChange: PropTypes.func,
-    autocomplete: PropTypes.string
+    autocomplete: PropTypes.string,
+    type: PropTypes.string,
+    error: PropTypes.bool,
+    success: PropTypes.bool,
 }
 
 export default Textbox
