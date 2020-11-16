33344eef1d509eaa32d15a4847d0ecf096319918
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-16 09:32:33 +0330

    fix StyledWrapper style

--- src/components/shared/pagination/pagination-item/pagination-item.styled.js
+++ src/components/shared/pagination/pagination-item/pagination-item.styled.js
@@ -5,11 +5,11 @@ export const StyledWrapper = styled.span`
   border-radius: ${({ theme }) => theme.defaults.borderRadius};
   margin: 0 ${({ theme }) => theme.dim[1]};
   padding: ${({ theme }) => theme.dim[1]} ${({ theme }) => theme.dim[2]};
-  background-color: ${({ theme, selectedPage }) => (selectedPage ? theme.palette.primary : theme.palette.back)};
-  color: ${({ theme, selectedPage }) => (selectedPage ? theme.palette.onPrimary : theme.palette.onSurface)};
+  background-color: ${({ theme, currentPage, text }) => (currentPage === text ? theme.palette.primary : theme.palette.back)};
+  color: ${({ theme, currentPage, text }) => (currentPage === text ? theme.palette.onPrimary : theme.palette.onSurface)};
   ${({ theme }) => theme.typography.bodyMdNormal};
   width: 41px;
   height: 40px;
   cursor: pointer;
-  transition:${({selectedPage})=> selectedPage ? '0.3s all':'0s all'} ;
+  transition: ${({ currentPage, text }) => (currentPage === text ? "0.3s all" : "0s all")};
 `;
