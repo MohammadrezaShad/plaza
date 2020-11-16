f9329df981787e3c00b90013a972dcf5fb5e796f
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-16 14:00:31 +0330

    fix StyledWrapper style

--- src/components/shared/loading-bar/loading-bar.styled.js
+++ src/components/shared/loading-bar/loading-bar.styled.js
@@ -7,17 +7,20 @@ export const StyledWrapper = styled.div`
   left: 0;
   height: 4px;
   overflow: hidden;
-  opacity: ${({ loaded, loading,reset }) => (loaded && loading === 100 && !reset ? 0 : 1)};
-  visibility: ${({ loaded, loading,reset }) => (loaded && loading === 100 && !reset ? "hidden" : "visable")};
-  transition: ${({ loaded }) => (loaded ? ".2s opacity .5s,0s visibility .5s;" : "0 opacity 0s,0 visibility 0;")};
+  opacity:${({loaded})=>loaded ? 0:1};
+  visibility:${({loaded})=>loaded ? 'hidden':'visible'};
+  transition:${({loaded})=> loaded ? '.2s opacity,0s visibility':'0s opacity,0s visibility'} ;
+  transition-delay: ${({ loaded }) => (loaded ? "1s , 1.2s" : "0s , 0s")};
   background-color: ${({ theme }) => theme.palette.stroke};
   z-index: 200;
   &::after {
     content: "";
     position: absolute;
     left: 0;
-    width: ${({ loading, reset, loaded }) => (!loaded && loading === 100 && reset ? 0 : `${loading}%`)};
-    transition: ${({ loading, reset, loaded }) => (loading === 100 && loaded ? ".2s width" : !loaded && loading === 100 && reset ? "0s width" : ".1s width")};
+    width: ${({ loading, reset, loaded }) => 
+    (reset && !loaded && loading === 100 ? 0 : `${loading}%`)};
+    transition: ${({ loading, reset, loaded }) => 
+    (reset && !loaded && loading === 100 ? "all 0s" : `all 1s`)};
     top: 0;
     bottom: 0;
     height: 5px;
