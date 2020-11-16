1483ebbc3cef4fdd66bf48a31f762b13e289fe06
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-16 13:52:57 +0330

    refactor LoadinBar time

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <ThemeProvider>
       <LoadingProvider>
-        <LoadingBar step={10} time={1000}/>
+        <LoadingBar step={10} time={500}/>
         {renderRoutes(routes)}
       </LoadingProvider>
     </ThemeProvider>
