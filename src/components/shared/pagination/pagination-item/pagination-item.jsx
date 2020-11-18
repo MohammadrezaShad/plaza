9827860a029065c6d730eb49f7713be745bafc41
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-18 08:39:26 +0330

    refactor pagination-item

--- src/components/shared/pagination/pagination-item/pagination-item.jsx
+++ src/components/shared/pagination/pagination-item/pagination-item.jsx
@@ -1,21 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-import { StyledWrapper } from './pagination-item.styled'
+import { StyledWrapper, StyledSeparator } from './pagination-item.styled'
 
-const PaginationItem = ({ text, currentPage, pager }) => {
+const PaginationItem = ({ text, currentPage, pager, hidden, sperator }) => {
+
+    const renderHandler = () => {
+        if (sperator) {
+            return <StyledSeparator>...</StyledSeparator>
+        } else if (!hidden) {
+            return (
+                <StyledWrapper text={text} currentPage={currentPage} onClick={() => pager(text)}>
+                    {text}
+                </StyledWrapper>
+            )
+
+        }
+    }
 
     return (
-        <StyledWrapper text={text} currentPage={currentPage} onClick={() => pager(text)}>
-            {text}
-        </StyledWrapper>
+        <Fragment>
+            {
+                renderHandler()
+            }
+        </Fragment>
+
     )
 }
 
 PaginationItem.propTypes = {
     text: PropTypes.number,
     currentPage: PropTypes.number,
-    pager: PropTypes.func
+    pager: PropTypes.func,
+    hidden: PropTypes.bool,
+    sperator: PropTypes.bool
 }
 
 export default PaginationItem
--- src/components/shared/pagination/pagination-item/pagination-item.styled.js
+++ src/components/shared/pagination/pagination-item/pagination-item.styled.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.span`
-  display: inline-flex;
+  display:inline-flex;
   border-radius: ${({ theme }) => theme.defaults.borderRadius};
+  justify-content:center;
+  align-items:center;
   margin: 0 ${({ theme }) => theme.dim[1]};
-  padding: ${({ theme }) => theme.dim[1]} ${({ theme }) => theme.dim[2]};
   background-color: ${({ theme, currentPage, text }) => (currentPage === text ? theme.palette.primary : theme.palette.back)};
   color: ${({ theme, currentPage, text }) => (currentPage === text ? theme.palette.onPrimary : theme.palette.onSurface)};
   ${({ theme }) => theme.typography.bodyMdNormal};
@@ -13,3 +14,16 @@ export const StyledWrapper = styled.span`
   cursor: pointer;
   transition: ${({ currentPage, text }) => (currentPage === text ? "0.3s all" : "0s all")};
 `;
+
+
+export const StyledSeparator=styled.span`
+ display:inline-flex;
+  border-radius: ${({ theme }) => theme.defaults.borderRadius};
+  justify-content:center;
+  align-items:center;
+  margin: 0 ${({ theme }) => theme.dim[1]};
+  background-color: ${({ theme, currentPage, text }) => (theme.palette.back)};
+  color: ${({ theme, currentPage, text }) => (theme.palette.onSurface)};
+  ${({ theme }) => theme.typography.bodyMdNormal};
+  height: 40px;
+`
