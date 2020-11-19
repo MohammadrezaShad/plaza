6bae2d36cc2a68d06eea20179aa6ee7aa8e2e0b8
MohammadReza ShadMobaraki <shadmobaraki@gilertebat.com>
2020-11-19 11:10:37 +0330

    fix footer-enamad styles

--- src/components/layouts/main-layout/mobile/footer/footer-enamad/footer-enamad.styled.js
+++ src/components/layouts/main-layout/mobile/footer/footer-enamad/footer-enamad.styled.js
@@ -4,6 +4,7 @@ export const StyledWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
 `;
 
 export const StyledLink = styled.a`
@@ -13,8 +14,16 @@ export const StyledLink = styled.a`
   width: 120px;
   height: 150px;
   border: 1px solid ${({ theme }) => theme.palette.stroke};
+  flex-shrink: 0;
   &:not(:first-child) {
     margin-right: ${({ theme }) => theme.dim[4]};
+  }
+  ${({ theme }) => theme.breakPoints.mobileMd} {
+    margin-right: 0 !important;
+    flex-basis: 100%;
+    &:not(:first-child){
+      margin-top:${({theme})=>theme.dim[2]};
+    }
   }
 `;
 
