17c1337b3541efaf4ee88ddd843633f52bf57a8b
unknown <shadmobaraki@plaza.ir>
2020-11-21 09:43:31 +0330

    add brands-sidebar

---
+++ src/components/views/brands/client/brands-sidebar/brands-sidebar.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import { StyledWrap } from './brands-sidebar.styled'
+
+const BrandsSidebar = props => {
+    return (
+        <Fragment>
+           SideBar
+        </Fragment>
+    )
+}
+
+BrandsSidebar.propTypes = {
+
+}
+
+export default BrandsSidebar
---
+++ src/components/views/brands/client/brands-sidebar/brands-sidebar.styled.js
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledWrap=styled.div``
