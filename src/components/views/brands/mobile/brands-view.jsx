1518840255348bdf2162979a00954c85c663ed7c
unknown <shadmobaraki@plaza.ir>
2020-11-21 09:43:47 +0330

    add brands-view

---
+++ src/components/views/brands/mobile/brands-view.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { StyledWrapper } from './brands-view.styled'
+
+const BrandsView = props => {
+    return (
+        <StyledWrapper>
+           mobileBrandsView
+        </StyledWrapper>
+    )
+}
+
+BrandsView.propTypes = {
+
+}
+
+export default BrandsView
