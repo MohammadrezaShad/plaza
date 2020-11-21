ae9019ceb3ac518a32abfdfd18808ec27148ffed
unknown <shadmobaraki@plaza.ir>
2020-11-21 09:43:16 +0330

    add brands-content

---
+++ src/components/views/brands/client/brands-content/brands-content.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+const BrandsContent = props => {
+    return (
+        <Fragment>
+            Content
+        </Fragment>
+    )
+}
+
+BrandsContent.propTypes = {
+
+}
+
+export default BrandsContent
