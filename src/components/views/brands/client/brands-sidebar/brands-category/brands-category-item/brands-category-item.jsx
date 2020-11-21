4fe1f91dac119b3485ae0824e59dc81bdeb96a7c
unknown <shadmobaraki@plaza.ir>
2020-11-21 14:46:25 +0330

    add init content

---
+++ src/components/views/brands/client/brands-sidebar/brands-category/brands-category-item/brands-category-item.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment, useState } from 'react'
+import PropTypes from 'prop-types'
+
+import {
+    StyledWrap,
+} from './brands-category-item.styled'
+import Checkbox from '../../../../../../shared/form/checkbox'
+
+
+const BrandsCategoryItem = ({ id,title,onChange}) => {
+
+    return (
+
+        <StyledWrap key={id}>
+            <Checkbox text={title} onChange={(e)=>onChange(id,e)}/>
+        </StyledWrap>
+    )
+}
+
+BrandsCategoryItem.propTypes = {
+    id:PropTypes.number,
+    title:PropTypes.string,
+    onChange:PropTypes.func
+}
+
+export default BrandsCategoryItem
