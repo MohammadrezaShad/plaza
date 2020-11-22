c7013161a5d563d8f08394789920e7f64284cb89
unknown <shadmobaraki@plaza.ir>
2020-11-22 10:40:13 +0330

    add init content

--- src/components/views/brands/client/brands-content/brands-content.jsx
+++ src/components/views/brands/client/brands-content/brands-content.jsx
@@ -1,10 +1,23 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
+import { StyledBlock } from './brands-content.styled'
+import BrandsContentTop from './brands-content-top'
+import BrandsContentMain from './brands-content-main'
+import BrandsContentBottom from './brands-content-bottom'
+
 const BrandsContent = props => {
     return (
         <Fragment>
-            Content
+            <StyledBlock>
+                <BrandsContentTop />
+            </StyledBlock>
+            <StyledBlock>
+                <BrandsContentMain />
+            </StyledBlock>
+            <StyledBlock>
+                <BrandsContentBottom />
+            </StyledBlock>
         </Fragment>
     )
 }
--- src/components/views/brands/client/brands-content/brands-content.styled.js
+++ src/components/views/brands/client/brands-content/brands-content.styled.js
@@ -1,2 +1,9 @@
 import styled from 'styled-components';
 
+
+export const StyledBlock=styled.div`
+&:first-child{
+    margin-bottom:${({theme})=>theme.dim[6]};
+    padding-bottom:${({theme})=>theme.dim[6]};
+}
+`
