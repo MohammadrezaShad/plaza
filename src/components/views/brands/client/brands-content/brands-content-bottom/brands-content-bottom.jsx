1d6c55205040938ea58feb12b939030411178ba8
unknown <shadmobaraki@plaza.ir>
2020-11-22 10:40:36 +0330

    add brands-content-bottom

---
+++ src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+const BrandsContentBottom = props => {
+    return (
+        <Fragment>
+            BrandsContentBottom
+        </Fragment>
+    )
+}
+
+BrandsContentBottom.propTypes = {
+
+}
+
+export default BrandsContentBottom
