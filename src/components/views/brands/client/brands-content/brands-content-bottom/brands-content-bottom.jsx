c8dd2acfd41aa627cb16fad8cc8e9779191bc9cb
unknown <shadmobaraki@plaza.ir>
2020-11-28 16:20:17 +0330

    add StyledDescription

--- src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.jsx
+++ src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.jsx
@@ -1,7 +1,14 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-import { StyledWrapper, StyledItem, StlyedText } from './brands-content-bottom.styled'
+import {
+    StyledWrapper,
+    StyledItem,
+    StlyedText,
+    StyledDescription,
+    StyledDescriptionTitle,
+    StyledDescriptionText
+} from './brands-content-bottom.styled'
 import Product from '../../../../../shared/product'
 import { ReactComponent as ProductIcon } from '../../../../../../assets/res/client/prod.svg';
 import SubProductSlider from '../../../../../shared/sub-product-slider';
@@ -10,9 +17,24 @@ const items = [{ title: "محافظ برق و چند راهی", id: 2 }, { title
 
 const BrandsContentBottom = props => {
     return (
-        <StyledWrapper>
-            <SubProductSlider title="کالاهای هویت" items={items} />
-        </StyledWrapper>
+        <Fragment>
+            <StyledWrapper>
+                <SubProductSlider title="کالاهای هویت" items={items} />
+            </StyledWrapper>
+            <StyledDescription>
+                <StyledDescriptionTitle>برند هویت</StyledDescriptionTitle>
+                <StyledDescriptionText>
+                    هویت؛ یک برند قدرتمند چینی
+                   <br />
+                کمپانی هویت (Havit) سال 1998 در گوانجو چین با هدف تولید تجهیزات الکترونیکی و کامپیوتری راه اندازی شد. در سال 2001 هویت برای اولین بار خط تولید خود را راه انداخت و محصولاتی به صورت محدود تولید و به بازار چین عرضه کرد. به مرور در سال های بعد با جا انداختن برند هویت در میان کاربران، هویت توانست به یکی از بزرگترین تولیدکنندگان لوازم جانبی دیجیتال در دنیا تبدیل شود. در سال 2017 میلادی محصولات برند هویت به سراسر دنیا صادر شد.
+
+                    <br />
+                    هویت (Havit) بخشی از تمرکز خود را روی تولید در دو حوزه گیمینگ و محصولات حرفه ای گذاشته است. محصولات گیمینگ هویت شامل هدست، ماوس و کیبورد گیمینگ می شود. از سوی دیگر، محصولات سری H مخصوص آن دسته از کاربرانی است که کیفیت برای آن ها اهمیت دارد. هدست، اسپیکر و هندزفری های سری H هویت برای ورزشکاران بسیار مناسب است.
+                    <br />
+                    این کمپانی بخش تولید خود را گسترش داد و لوازم جانبی موبایل را به لیست محصولاتش افزود. هویت با توسعه بخش مهندسی و بازاریابی، محصولات با قابلیت های عالی و طراحی های خاص تولید نمود و برند خود را در بازارهای جهانی معروف کرد. این کمپانی در بخش لوازم جانبی موبایل، حضور جدی تری دارد و هدفون،اسپیکر، کابل، شارژر ، دستگاه های بی سیم ، هوشمند و ... از محصولات متنوع آن است
+                </StyledDescriptionText>
+            </StyledDescription>
+        </Fragment>
     )
 }
 
--- src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.styled.js
+++ src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.styled.js
@@ -26,3 +26,24 @@ export const StlyedText = styled.div`
   text-align:center;
   margin-top:${({ theme }) => theme.dim[3]};
 `
+
+export const StyledDescription = styled.div`
+padding:${({ theme }) => theme.dim[4]};
+ border:1px solid ${({ theme }) => theme.palette.stroke};
+ margin-top:${({ theme }) => theme.dim[4]};
+`
+
+export const StyledDescriptionTitle = styled.span`
+  display:inline-block;
+  color:${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.bodyLgBold};
+  margin-bottom:${({ theme }) => theme.dim[2]};
+  line-height:1.4;
+`
+
+export const StyledDescriptionText = styled.p`
+  display:inline-block;
+  color:${({ theme }) => theme.palette.onSurface};
+  ${({ theme }) => theme.typography.bodyMdNormal};
+  line-height:1.9;
+`
