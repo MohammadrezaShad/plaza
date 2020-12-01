07ec32a1f0a322431322ca41e0a940b6253b6232
unknown <shadmobaraki@plaza.ir>
2020-12-01 15:41:08 +0330

    add brands-products

---
+++ src/components/views/brands/mobile/brands-content-main/brands-products/brands-products.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BrandsProducts = props => {
+    return (
+        <div>
+            BrandsProducts
+        </div>
+    )
+}
+
+BrandsProducts.propTypes = {
+
+}
+
+export default BrandsProducts
