a24f4d081b341f240581f3ea2490f14b0033a27c
unknown <shadmobaraki@plaza.ir>
2020-12-01 15:34:41 +0330

    wip StyledItem

--- src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.jsx
+++ src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.jsx
@@ -3,15 +3,11 @@ import PropTypes from 'prop-types'
 
 import {
     StyledWrapper,
-    StyledItem,
-    StlyedText,
     StyledDescription,
     StyledDescriptionTitle,
     StyledDescriptionText
 } from './brands-content-bottom.styled'
-import Product from '../../../../../shared/product'
-import { ReactComponent as ProductIcon } from '../../../../../../assets/res/client/prod.svg';
-import SubProductSlider from '../../../../../shared/sub-product-slider';
+import SubProductSlider from './sub-product-slider'
 
 const items = [{ title: "محافظ برق و چند راهی", id: 2 }, { title: "موس", id: 3 }, { title: "کیبورد", id: 4 }, { title: "کابل اندرویدی", id: 5 }, { title: "محافظ برق و چند راهی", id: 6 }, { title: "موس", id: 7 }, { title: "کیبورد", id: 8 }, { title: "کابل اندرویدی", id: 9 }, { title: "محافظ برق و چند راهی", id: 10 }, { title: "موس", id: 11 }, { title: "کیبورد", id: 12 }, { title: "کابل اندرویدی", id: 13 }, { title: "محافظ برق و چند راهی", id: 14 }, { title: "موس", id: 15 }, { title: "کیبورد", id: 16 }, { title: "کابل اندرویدی", id: 17 }, { title: "محافظ برق و چند راهی", id: 18 }, { title: "موس", id: 20 }, { title: "کیبورد", id: 19 }]
 
--- src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.styled.js
+++ src/components/views/brands/client/brands-content/brands-content-bottom/brands-content-bottom.styled.js
@@ -5,27 +5,6 @@ export const StyledWrapper = styled.div`
  background-color:${({ theme }) => theme.palette.surface};
 `
 
-export const StyledItem = styled.div`
-  display:flex;
-  flex-flow:column;
-  align-items:center;
-  justify-content:center;
-  background-color:${({ theme }) => theme.palette.back};
-  border-radius:${({ theme }) => theme.defaults.borderRadius};
-  border:1px solid ${({ theme }) => theme.palette.stroke};
-  width:109px;
-  flex-shrink:0;
-  height:160px;
-  &:not(:last-child){
-    margin-left:${({ theme }) => theme.dim[3]};
-  }
-`
-
-export const StlyedText = styled.div`
-  display:inline-block;
-  text-align:center;
-  margin-top:${({ theme }) => theme.dim[3]};
-`
 
 export const StyledDescription = styled.div`
 padding:${({ theme }) => theme.dim[4]};
