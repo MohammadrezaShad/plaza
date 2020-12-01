65d343a63e9e3bc6bce84657975d0dccd3a8a0c6
unknown <shadmobaraki@plaza.ir>
2020-12-01 15:39:51 +0330

    add brands-feedback

---
+++ src/components/views/brands/mobile/brands-content-main/brands-feedback/brands-feedback.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BrandsFeedback = props => {
+    return (
+        <div>
+            BrandsFeedback
+        </div>
+    )
+}
+
+BrandsFeedback.propTypes = {
+
+}
+
+export default BrandsFeedback
