811b1e63f2f084df05d161f9b549715bb86c5275
unknown <shadmobaraki@plaza.ir>
2020-12-01 15:41:24 +0330

    refactor content

--- src/components/views/club/home/client/home-bottom/home-bottom.styled.js
+++ src/components/views/club/home/client/home-bottom/home-bottom.styled.js
@@ -2,10 +2,8 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.div`
   margin-top: ${({ theme }) => theme.dim[3]};
-
 `
 
 export const StyledWrapper = styled.div`
   padding:${({ theme }) => theme.dim[4]};
-  padding-bottom:${({ theme }) => theme.dim[1]};
 `;
