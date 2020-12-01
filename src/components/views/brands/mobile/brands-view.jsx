28b834f1856ee84b50ae781cebba799c2a2f4fa8
unknown <shadmobaraki@plaza.ir>
2020-12-01 15:38:03 +0330

    add content

--- src/components/views/brands/mobile/brands-view.jsx
+++ src/components/views/brands/mobile/brands-view.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { StyledWrapper } from './brands-view.styled'
+import { StyledWrapper, StyledBlock, StyledBottomWrap, StyledContent } from './brands-view.styled'
+import BrandsPreview from '../../../shared/brands-preview'
+import BrandsContentBottom from './brands-content-bottom'
+import BrandsContentMain from './brands-content-main/brands-content-main'
+
+const items = [{ title: "محافظ برق و چند راهی", id: 2 }, { title: "موس", id: 3 }, { title: "کیبورد", id: 4 }, { title: "کابل اندرویدی", id: 5 }, { title: "محافظ برق و چند راهی", id: 6 }, { title: "موس", id: 7 }, { title: "کیبورد", id: 8 }, { title: "کابل اندرویدی", id: 9 }, { title: "محافظ برق و چند راهی", id: 10 }, { title: "موس", id: 11 }, { title: "کیبورد", id: 12 }, { title: "کابل اندرویدی", id: 13 }, { title: "محافظ برق و چند راهی", id: 14 }, { title: "موس", id: 15 }, { title: "کیبورد", id: 16 }, { title: "کابل اندرویدی", id: 17 }, { title: "محافظ برق و چند راهی", id: 18 }, { title: "موس", id: 20 }, { title: "کیبورد", id: 19 }]
 
 const BrandsView = props => {
     return (
         <StyledWrapper>
-           mobileBrandsView
+            <StyledBlock>
+                <BrandsPreview />
+            </StyledBlock>
+            <StyledContent>
+                <BrandsContentMain />
+            </StyledContent>
+            <StyledBottomWrap>
+                <BrandsContentBottom />
+            </StyledBottomWrap>
         </StyledWrapper>
     )
 }
--- src/components/views/brands/mobile/brands-view.styled.js
+++ src/components/views/brands/mobile/brands-view.styled.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
-export const StyledWrapper=styled.div`
+export const StyledWrapper = styled.div`
+  padding: ${({ theme }) => theme.defaults.screenSideDim};
 
+`
+
+export const StyledBlock = styled.div`
+ padding:${({ theme }) => theme.dim[4]};
+ border:1px solid ${({ theme }) => theme.palette.stroke};
+`
+
+export const StyledBottomWrap = styled.div`
+ margin-top:${({ theme }) => theme.dim[2]};
+
+`
+
+export const StyledContent = styled.div`
+margin-top:${({ theme }) => theme.dim[4]};
 `
