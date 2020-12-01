5e727e474bff1c5aaead680e4ab26004e5d2d682
unknown <shadmobaraki@plaza.ir>
2020-12-01 15:40:55 +0330

    add brands-filters

---
+++ src/components/views/brands/mobile/brands-content-main/brands-filters/brands-filters.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BrandsFilters = props => {
+    return (
+        <div>
+            BrandsFilters
+        </div>
+    )
+}
+
+BrandsFilters.propTypes = {
+
+}
+
+export default BrandsFilters
