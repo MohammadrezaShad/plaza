424253f97e3649bb3e08ed996514cb4c88af2c76
unknown <shadmobaraki@plaza.ir>
2020-12-02 16:00:59 +0330

    add content

--- src/components/views/brands/mobile/brands-content-main/brands-feedback/brands-feedback.jsx
+++ src/components/views/brands/mobile/brands-content-main/brands-feedback/brands-feedback.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-const BrandsFeedback = props => {
+
+import {StyledFeedbackWrap} from './brands-feedback.styled'
+import FeedbackItem from '../../../../../shared/feedback-item'
+
+const BrandsFeedback = (
+    {
+        items = [{ name: "پرهام گیلانی", title: "پایه شارژ دسته بازی DualShock 4 مدل Dual Charging", date: "‏14 تیر 1399، 20:00", recommendetion: true, id: 1 }, { name: "پرهام گیلانی", title: "پایه شارژ دسته بازی DualShock 4 مدل Dual Charging", date: "‏14 تیر 1399، 20:00", recommendetion: false, id: 2 }]
+    }
+) => {
     return (
-        <div>
-            BrandsFeedback
-        </div>
+        <Fragment>
+            {
+                items.map(({ id, name, date, recommendetion, title }) => {
+                    return (
+                        <StyledFeedbackWrap   key={id}>
+                            <FeedbackItem
+                                name={name}
+                                title={title}
+                                date={date}
+                                recommendetion={recommendetion} 
+                                mobile={true}
+                                />
+                        </StyledFeedbackWrap>
+                    )
+                })
+            }
+        </Fragment>
     )
 }
 
 BrandsFeedback.propTypes = {
-
+    items: PropTypes.array
 }
 
 export default BrandsFeedback
