bb046cd8f7f225dbda0f84c8c911ab4e45baad72
unknown <shadmobaraki@plaza.ir>
2020-12-06 13:44:20 +0330

    add content

--- src/components/views/brands/mobile/brands-content-main/brands-content-main.jsx
+++ src/components/views/brands/mobile/brands-content-main/brands-content-main.jsx
@@ -1,20 +1,33 @@
-import React, { Fragment, useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { StyledBlock, StyledTabWrap, StyledFeedWrap } from './brands-content-main.styled'
+import { StyledBlock, StyledTabWrap, StyledFeedWrap, StyledLoadingIcon, StyledExpandButton } from './brands-content-main.styled'
 import Tabs from '../../../../shared/tabs'
 import BrandsProducts from './brands-products'
 import BrandsFeedback from './brands-feedback'
 import BrandsFilters from './brands-filters'
+import { ReactComponent as Loading } from '../../../../../assets/images/loading.svg';
+import Button from '../../../../shared/button'
+import { buttonVariants } from '../../../../../constants/button-configs'
+import useScrollRequest from '../../../../../helpers/use-scroll-request'
 
-const BrandsContentMain = props => {
+const BrandsContentMain = () => {
     const { t } = useTranslation()
+    const node = useRef(null)
     const [selectedTab, setselectedTab] = useState(0)
+    const [loadingCount, setLoadingCount] = useState(0)
+    const loadData = () => {
+        console.log('LoadData')
+        setLoadingCount(loadingCount + 1)
+    }
+    const [elementInView, elementGrabber] = useScrollRequest(2000, loadingCount !== 0 && loadingCount % 2 == 0, loadData)
     const tabClickHandler = (tabIndex) => {
         setselectedTab(tabIndex)
     }
-
+    console.log(loadingCount)
+    useEffect(() => {
+        elementGrabber(node.current)
+    }, [])
     return (
         <Fragment>
             <StyledBlock>
@@ -28,6 +41,15 @@ const BrandsContentMain = props => {
                     <StyledTabWrap>
                         <BrandsFilters />
                         <BrandsProducts />
+                        <StyledLoadingIcon ref={node}>
+                            {
+                                loadingCount !== 0 && loadingCount % 2 == 0 ?
+                                    <Button variant={buttonVariants.OUTLINE}
+                                        text={t("brandsView.showMoreProduct")}
+                                        onClick={loadData} /> :
+                                    <Loading />
+                            }
+                        </StyledLoadingIcon>
                     </StyledTabWrap> :
                     <StyledFeedWrap>
                         <BrandsFeedback />
--- src/components/views/brands/mobile/brands-content-main/brands-content-main.styled.js
+++ src/components/views/brands/mobile/brands-content-main/brands-content-main.styled.js
@@ -1,10 +1,29 @@
-import styled from 'styled-components';
+import styled, { keyframes } from "styled-components";
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
 
-export const StyledBlock=styled.div`
- margin-bottom:${({theme})=>theme.dim[2]};
-`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+export const StyledBlock = styled.div`
+  margin-bottom: ${({ theme }) => theme.dim[2]};
+`;
 
-export const StyledTabWrap=styled.div``
+export const StyledTabWrap = styled.div``;
 
+export const StyledFeedWrap = styled.div``;
 
-export const StyledFeedWrap=styled.div``
+export const StyledLoadingIcon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${({ theme }) => theme.dim[2]};
+  svg {
+    animation: ${rotate} 2s linear infinite;
+  }
+`;
+
+export const StyledExpandButton = styled.div``;
