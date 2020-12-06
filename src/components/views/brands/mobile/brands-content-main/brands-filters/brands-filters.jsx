37ba071e4c4a85ff9e9a5dc1989a4df71af454d3
unknown <shadmobaraki@plaza.ir>
2020-12-06 13:36:36 +0330

    add content

--- src/components/views/brands/mobile/brands-content-main/brands-filters/brands-filters.jsx
+++ src/components/views/brands/mobile/brands-content-main/brands-filters/brands-filters.jsx
@@ -1,26 +1,51 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useState } from 'react'
 
 import { StyledBlock, StyledFilterBadge } from './brands-filters.styled'
 import BrandsFiltersTop from './brands-filters-top'
 import BrandsFiltersBadge from './brands-filters-badge'
 
-const BrandsFilters = props => {
+const BrandsFilters = () => {
+    const [activeFilters, setActiveFilters] = useState([])
+    const [activeFiltersMain, setactiveFiltersMain] = useState([])
+
+    const onChange = (id, text) => {
+        let newActiveFilters = [...activeFilters, { text, id }]
+        if (activeFilters.find((activeFilter) => activeFilter.id === id)) {
+            newActiveFilters = newActiveFilters.filter((newActiveFilter) => newActiveFilter.id !== id)
+        }
+        setActiveFilters([...newActiveFilters])
+    }
+
+    const onClick = () => {
+        setactiveFiltersMain([...activeFilters])
+    }
+
+    const onChangeLive = (id, text) => {
+        let newActiveFilters = [...activeFilters, { text, id }]
+        if (activeFilters.find((activeFilter) => activeFilter.id === id)) {
+            newActiveFilters = newActiveFilters.filter((newActiveFilter) => newActiveFilter.id !== id)
+        }
+        setActiveFilters([...newActiveFilters])
+        setactiveFiltersMain([...newActiveFilters])
+    }
+
     return (
         <Fragment>
-            <StyledBlock>
-                <BrandsFiltersTop />
+            <StyledBlock acactiveFiltersMain={activeFiltersMain.length}>
+                <BrandsFiltersTop
+                    activeFiltersMain={activeFiltersMain}
+                    onChange={onChange}
+                    onClick={onClick}
+                />
             </StyledBlock>
             <StyledBlock>
-                <StyledFilterBadge>
-                    <BrandsFiltersBadge text="دسته بازی" />
-                </StyledFilterBadge>
-                <StyledFilterBadge>
-                    <BrandsFiltersBadge text="دسته بازی" />
-                </StyledFilterBadge>
-                <StyledFilterBadge>
-                    <BrandsFiltersBadge text="دسته بازی" />
-                </StyledFilterBadge>
+                {
+                    activeFiltersMain.map(({ id, text }) => {
+                        return <StyledFilterBadge key={id}>
+                            <BrandsFiltersBadge onClick={onChangeLive} text={text} id={id} />
+                        </StyledFilterBadge>
+                    })
+                }
             </StyledBlock>
         </Fragment>
     )
