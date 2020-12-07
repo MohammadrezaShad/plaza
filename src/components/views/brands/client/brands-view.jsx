76282d658dcd3225739cdb1a753f299cee322378
unknown <shadmobaraki@plaza.ir>
2020-12-07 11:05:09 +0330

    refactor content

--- src/components/views/brands/client/brands-view.jsx
+++ src/components/views/brands/client/brands-view.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { 
-    StyledWrapper, 
-    StyledLayout, 
-    StyleContentdBlock, 
-    StyledSidebarBlock, 
-    StyledContainer 
+import {
+    StyledWrapper,
+    StyledLayout,
+    StyleContentdBlock,
+    StyledSidebarBlock,
+    StyledContainer,
+    StyledPreview,
+    StyledContent,
+    StyledBlock,
+    StyledPreviewWrap,
+    StyledTopWrap,
+    StyledTop
 } from './brands-view.styled'
 import BrandsSidebar from './brands-sidebar'
 import BrandsContent from './brands-content'
+import BrandsContentTop from './brands-content/brands-content-top'
+import BrandsPreview from '../../../shared/brands-preview'
 
 const BrandsView = props => {
     return (
         <StyledWrapper>
             <StyledLayout>
                 <StyledContainer>
-                    <StyledSidebarBlock>
-                        <BrandsSidebar />
-                    </StyledSidebarBlock>
+                    <StyledBlock>
+                        <StyledPreviewWrap>
+                            <StyledPreview>
+                                <BrandsPreview />
+                            </StyledPreview>
+                        </StyledPreviewWrap>
+                        <StyledTopWrap>
+                            <StyledTop>
+                                <BrandsContentTop />
+                            </StyledTop>
+                        </StyledTopWrap>
+                    </StyledBlock>
                     <StyleContentdBlock>
-                        <BrandsContent />
+                        <StyledSidebarBlock>
+                            <BrandsSidebar />
+                        </StyledSidebarBlock>
+                        <StyledContent>
+                            <BrandsContent />
+                        </StyledContent>
                     </StyleContentdBlock>
                 </StyledContainer>
             </StyledLayout>
