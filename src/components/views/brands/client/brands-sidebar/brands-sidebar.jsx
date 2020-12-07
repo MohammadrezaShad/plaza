6c543f5680c7ea4fbb14c232eac75fefcd089441
unknown <shadmobaraki@plaza.ir>
2020-12-07 11:06:00 +0330

    refactor content

--- src/components/views/brands/client/brands-sidebar/brands-sidebar.jsx
+++ src/components/views/brands/client/brands-sidebar/brands-sidebar.jsx
@@ -1,20 +1,15 @@
 import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
 
 import { StyledBlock } from './brands-sidebar.styled'
 import BrandsCategory from './brands-category'
-import Checkbox from '../../../../shared/form/checkbox'
 import SwitchButton from '../../../../shared/form/switch-button'
 import { useTranslation } from 'react-i18next'
 import BrandsPreview from '../../../../shared/brands-preview'
 
-const BrandsSidebar = props => {
+const BrandsSidebar = () => {
     const { t } = useTranslation()
     return (
         <Fragment>
-            <StyledBlock>
-                <BrandsPreview />
-            </StyledBlock>
             <StyledBlock>
                 <SwitchButton text={t("brandsView.showAvailableProducts")} />
             </StyledBlock>
--- src/components/views/brands/client/brands-sidebar/brands-sidebar.styled.js
+++ src/components/views/brands/client/brands-sidebar/brands-sidebar.styled.js
@@ -1,12 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledBlock = styled.div`
- padding:${({ theme }) => theme.dim[4]};
- &:first-child{
-     border:1px solid ${({ theme }) => theme.palette.stroke};
- }
- &:not(:first-child):not(:last-child){
-    border-bottom:1px solid ${({ theme }) => theme.palette.stroke};
-
- }
-`
+  padding: ${({ theme }) => theme.dim[4]} 0;
+  &:first-child {
+    border-bottom: 1px solid ${({ theme }) => theme.palette.stroke};
+  }
+`;
