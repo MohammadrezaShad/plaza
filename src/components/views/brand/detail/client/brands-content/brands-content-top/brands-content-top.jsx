51349c00e8ec5289d704179cd23b55cfefa0437a
unknown <shadmobaraki@plaza.ir>
2020-12-08 11:24:25 +0330

    add brands-content-top

---
+++ src/components/views/brand/detail/client/brands-content/brands-content-top/brands-content-top.jsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import {
+    StyledTitle,
+    StyledContent,
+    StyledContainer,
+    StyledLink
+} from './brands-content-top.styled'
+import Product from '../../../../../../shared/product'
+import Button from '../../../../../../shared/button'
+import { buttonColors, buttonVariants } from '../../../../../../../constants/button-configs'
+import Paths from '../../../../../../../utils/paths'
+
+const BrandsContentTop = props => {
+    return (
+        <Fragment>
+            <StyledContainer>
+                <Product title={
+                    <Fragment>
+                        <StyledTitle>جدیدترین های هویت</StyledTitle>
+                        <StyledLink to={Paths.home.getPath()}>
+                            <Button
+                                text="نمایش بیشتر"
+                                variant={buttonVariants.LINK}
+                                color={buttonColors.SECONDARY}
+                                to={Paths.home.getPath()} />
+                        </StyledLink>
+                    </Fragment>
+                }
+                    borderTitle={false}
+                    marginHead={false}
+                    buttonTopLeft={true}
+                    alignRight={true}
+                    imgWidth="66%"
+                />
+            </StyledContainer>
+        </Fragment>
+    )
+}
+
+BrandsContentTop.propTypes = {
+
+}
+
+export default BrandsContentTop
