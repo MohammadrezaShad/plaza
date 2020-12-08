34393920addf98bb2c504182ae10cd51a2ad0382
unknown <shadmobaraki@plaza.ir>
2020-12-08 11:19:37 +0330

    add content

--- src/pages/brand/[slug].jsx
+++ src/pages/brand/[slug].jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Helmet } from "react-helmet";
 import loadable from '@loadable/component';
 
 import withDeviceDetection from '../../hoc/with-device-detection';
 import { DeviceTypes } from '../../utils/device-detection';
+import LoadingContext from '../../context/loading-context';
+import useLoad from '../../helpers/use-load';
 
-// const Client = loadable(() => import('../components/views/home/client'));
-// const Mobile = loadable(() => import('../components/views/home/mobile'));
+const Client = loadable(() => import('../../components/views/brand/detail/client'));
+const Mobile = loadable(() => import('../../components/views/brand/detail/mobile'));
 
 
-const BrandDetail = ({deviceType}) => (
-    <div>
-        <Helmet>
-            <title>{process.env.REACT_APP_NAME + ' | صفحه برند'}</title>
-        </Helmet>
-
-        <h1>barnd detail</h1>
-    </div>
-)
-
+const BrandDetail = ({ deviceType }) => {
+    const { loadedAction, resetLoaded, loaded } = useContext(LoadingContext)
+    const loadHook = useLoad([Client, Mobile], loadedAction, resetLoaded, loaded)
+    return (
+        <Fragment>
+            <Helmet>
+                <title>{process.env.REACT_APP_NAME + ' | صفحه ی برندها'}</title>
+            </Helmet>
+            {
+                deviceType == DeviceTypes.MOBILE
+                    ?
+                    <Mobile />
+                    :
+                    <Client />
+            }
+        </Fragment>
+    )
+}
 export default withDeviceDetection(BrandDetail);
